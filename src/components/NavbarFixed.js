43a87e2bb99c4a9f741194e3ebfbc9d982cdc8e8
Gustavo <grodriguez@contextglobal.com>
2022-08-17 17:06:49 -0400

    route path assigned to every element

--- src/components/NavbarFixed.js
+++ src/components/NavbarFixed.js
@@ -7,27 +7,27 @@ export const NavbarFixed = () => {
   return (
     <div>
       <nav className="navbar-fixed">
-        <Link to="/">
-          <div className='icon-span'>
-          <i className="bi bi-headset"></i>
+        <Link to="/request_devices">
+          <div className="icon-span">
+            <i className="bi bi-headset"></i>
             <span>REQUEST DEVICES</span>
           </div>
         </Link>
-        <Link to='/moreInfo'>
-          <div className='icon-span'>
-          <i className="bi bi-info-circle"></i>
+        <Link to="/more_info">
+          <div className="icon-span">
+            <i className="bi bi-info-circle"></i>
             <span>MORE INFO</span>
           </div>
         </Link>
-        <Link to='/event'>
-          <div className='icon-span'>
-          <i className="bi bi-calendar2-event"></i>
+        <Link to="/event_schedule">
+          <div className="icon-span">
+            <i className="bi bi-calendar2-event"></i>
             <span>EVENT SCHEDULED</span>
           </div>
         </Link>
-        <Link to='/profile'>
-          <div className='icon-span'>
-          <i className="bi bi-person"></i>
+        <Link to="/my_profile">
+          <div className="icon-span">
+            <i className="bi bi-person"></i>
             <span>MY PROFILE</span>
           </div>
         </Link>
