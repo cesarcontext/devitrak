20d7e691dab6f3e43fa53fb8d76ec28cb8307f89
Gustavo <grodriguez@contextglobal.com>
2022-08-18 17:11:41 -0400

    profile section created

--- src/page/MyProfile.js
+++ src/page/MyProfile.js
@@ -1,8 +1,14 @@
 import React from "react";
-import { useContactInfoStore } from "../hooks/useContactInfoStore";
+import { useDeviceCount } from "../hooks/useDeviceCount";
 
 export const MyProfile = () => {
-  const { user } = useContactInfoStore();
+  const { deviceArray } = useDeviceCount();
+
+  const deviceSelected = localStorage.getItem("device");
+
+  const checking = localStorage.getItem("user");
+
+  const userParseStored = [JSON.parse(checking)];
 
   return (
     <div
@@ -14,19 +20,25 @@ export const MyProfile = () => {
         alignItem: "center",
       }}
     >
-      <div>
+      <div
+        style={{
+          margin: "20px",
+        }}
+      >
         <div>
           <h3>Account</h3>
         </div>
-        <div style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItem: "center"
-        }}>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItem: "center",
+          }}
+        >
           <div
             style={{
-              width: "75%",
-              height: "50%",
+              width: "30%",
+              height: "80%",
               margin: "0 auto",
               border: "solid 1px #212529",
               borderRadius: "15px",
@@ -57,10 +69,15 @@ export const MyProfile = () => {
             <div>
               <h5>Your conntact infomartion</h5>
             </div>
-            {user.map((item) => {
+            {userParseStored?.map((item) => {
               return (
                 <div>
-                  <div>
+                  <div
+                    style={{
+                      display: "flex",
+                      justifyContent: "center",
+                    }}
+                  >
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       width="16"
@@ -110,52 +127,144 @@ export const MyProfile = () => {
             <div>
               <h5>Your payment Informacion</h5>
             </div>
-            <div>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                className="bi bi-credit-card"
-                viewBox="0 0 16 16"
-              >
-                <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm2-1a1 1 0 0 0-1 1v1h14V4a1 1 0 0 0-1-1H2zm13 4H1v5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V7z" />
-                <path d="M2 10a1 1 0 0 1 1-1h1a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1v-1z" />
-              </svg>
-              <span> ***********5785</span>
-            </div>
-            <div>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                className="bi bi-calendar-date"
-                viewBox="0 0 16 16"
-              >
-                <path d="M6.445 11.688V6.354h-.633A12.6 12.6 0 0 0 4.5 7.16v.695c.375-.257.969-.62 1.258-.777h.012v4.61h.675zm1.188-1.305c.047.64.594 1.406 1.703 1.406 1.258 0 2-1.066 2-2.871 0-1.934-.781-2.668-1.953-2.668-.926 0-1.797.672-1.797 1.809 0 1.16.824 1.77 1.676 1.77.746 0 1.23-.376 1.383-.79h.027c-.004 1.316-.461 2.164-1.305 2.164-.664 0-1.008-.45-1.05-.82h-.684zm2.953-2.317c0 .696-.559 1.18-1.184 1.18-.601 0-1.144-.383-1.144-1.2 0-.823.582-1.21 1.168-1.21.633 0 1.16.398 1.16 1.23z" />
-                <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
-              </svg>
-              <span> 05/2025</span>
-            </div>
-            <div>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                className="bi bi-geo-alt"
-                viewBox="0 0 16 16"
-              >
-                <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z" />
-                <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
-              </svg>
-              <span> 55685</span>
+            <div
+              style={{
+                margin: "15px auto",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "right",
+                alignItems: "center",
+                width: "60%",
+                textAlign: "left",
+              }}
+            >
+              <div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-credit-card"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm2-1a1 1 0 0 0-1 1v1h14V4a1 1 0 0 0-1-1H2zm13 4H1v5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V7z" />
+                  <path d="M2 10a1 1 0 0 1 1-1h1a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1v-1z" />
+                </svg>
+                <span> ***********5785</span>
+              </div>
+              <div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-calendar-date"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M6.445 11.688V6.354h-.633A12.6 12.6 0 0 0 4.5 7.16v.695c.375-.257.969-.62 1.258-.777h.012v4.61h.675zm1.188-1.305c.047.64.594 1.406 1.703 1.406 1.258 0 2-1.066 2-2.871 0-1.934-.781-2.668-1.953-2.668-.926 0-1.797.672-1.797 1.809 0 1.16.824 1.77 1.676 1.77.746 0 1.23-.376 1.383-.79h.027c-.004 1.316-.461 2.164-1.305 2.164-.664 0-1.008-.45-1.05-.82h-.684zm2.953-2.317c0 .696-.559 1.18-1.184 1.18-.601 0-1.144-.383-1.144-1.2 0-.823.582-1.21 1.168-1.21.633 0 1.16.398 1.16 1.23z" />
+                  <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
+                </svg>
+                <span> 05/2025</span>
+              </div>
+              <div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-geo-alt"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z" />
+                  <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                </svg>
+                <span> 55685</span>
+              </div>
             </div>
           </div>
         </div>
-        <div>//*alert here*//</div>
-        <div>/**button to contact context global **/</div>
+        <div style={{ width: "40%", margin: "auto" }}>
+          {deviceSelected > 0 ? (
+            <div style={{ width: "100%", margin: "auto" }}>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  margin: "25px",
+                }}
+                className="alert alert-danger d-flex align-items-center"
+                role="alert"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="50"
+                  height="35"
+                  fill="currentColor"
+                  class="bi bi-exclamation-triangle"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M7.938 2.016A.13.13 0 0 1 8.002 2a.13.13 0 0 1 .063.016.146.146 0 0 1 .054.057l6.857 11.667c.036.06.035.124.002.183a.163.163 0 0 1-.054.06.116.116 0 0 1-.066.017H1.146a.115.115 0 0 1-.066-.017.163.163 0 0 1-.054-.06.176.176 0 0 1 .002-.183L7.884 2.073a.147.147 0 0 1 .054-.057zm1.044-.45a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566z" />
+                  <path d="M7.002 12a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 5.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995z" />
+                </svg>
+                <div>
+                  <h4>
+                    You need to return {deviceSelected}{" "}
+                    {deviceSelected > 1 ? "devices" : "device"}
+                  </h4>
+                  <span>
+                    You have 3 days remaining. <br />Devices not returned within 3
+                    days will be charged to your credit card on file.
+                  </span>
+                </div>
+              </div>
+            </div>
+          ) : (
+            <div
+              class="alert alert-success d-flex align-items-center"
+              role="alert"
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                margin: "25px",
+              }}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="50"
+                height="35"
+                fill="currentColor"
+                class="bi bi-check-circle-fill"
+                viewBox="0 0 16 16"
+              >
+                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+              </svg>
+              <div>
+                <h4>
+                  You have {deviceSelected} pending {" "}
+                  {deviceSelected > 1 ? "devices" : "device"}
+                </h4>
+                <span>
+                  You have returned all your devices.
+                </span>
+              </div>
+            </div>
+          )}
+        </div>
+        <div>
+          <button
+            style={{
+              margin: "auto",
+              backgroundColor: "rgba(69, 104, 220, 1)",
+              color: "#ffff",
+              height: "5vh",
+              borderRadius: "10px",
+              outline: "transparency",
+              border: "rgba(69, 104, 220, 1)",
+            }}
+          >
+            Contact Context Glocal
+          </button>
+        </div>
         <div
           style={{
             width: "55%",
@@ -167,7 +276,9 @@ export const MyProfile = () => {
             <h3>Your devices</h3>
             <span>All the devices with checkmarks are now returned</span>
           </div>
-          <div>
+          <div
+            style={{ width: "50%", margin: "auto", border: "solid 1px #fff" }}
+          >
             <div className="accordion" id="accordionExample">
               <div className="accordion-item">
                 <h2 className="accordion-header" id="headingOne">
@@ -189,9 +300,14 @@ export const MyProfile = () => {
                   data-bs-parent="#accordionExample"
                 >
                   <div className="accordion-body">
-                    <li>1</li>
-                    <li>2</li>
-                    <li>3</li>
+                    {deviceArray.map((item, index) => {
+                      return (
+                        <div key={index}>
+                          {item}
+                          <p>1</p>
+                        </div>
+                      );
+                    })}
                   </div>
                 </div>
               </div>
