f193f5a022158d7d015f5c70395a6cc5fb68e7d1
Gustavo <grodriguez@contextglobal.com>
2022-08-18 17:06:07 -0400

    slice to store reducer for contact info created

---
+++ src/store/slices/contactInfoSlice.js
@@ -0,0 +1,35 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const user = {
+    _id: new Date().getTime(),
+    groupName: "",
+    name: "",
+    email: "",
+    phoneNumber: "",
+};
+
+const contactInfoSlice = createSlice({
+  name: "contactInfo",
+  initialState: {
+    user: [ user ],
+  },
+  reducers: {
+    onAddNewContact: (state, { payload }) => {
+      state.user.push( payload );
+      state.user.shift();
+    },
+    onUpdateEvent: (state, { payload }) => {
+      state.user = state.user.map((contact) => {
+        if (contact._id === payload._id) {
+          return payload;
+        }
+        return contact;
+      });
+    },
+  },
+});
+
+export default contactInfoSlice.reducer;
+
+// Action creators are generated for each case reducer function
+export const { onAddNewContact, onUpdateContact } = contactInfoSlice.actions;
