09c086c804545769046f27d8b27ea92ee6dd2244
Gustavo <grodriguez@contextglobal.com>
2022-08-18 17:04:00 -0400

    image removed and title added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,14 +2,41 @@ import React from "react";
 
 export const Navbar = () => {
   return (
-    <nav style={{ display: 'flex', justifyContent: 'space-between', backgroundColor: 'rgba(30, 115, 190, 1)', color: '#fff'}}>
-      <div>
-        <img src="https://picsum.photos/100" alt="" />
+    <nav
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        backgroundColor: "rgba(30, 115, 190, 1)",
+        color: "#fff",
+      }}
+    >
+      <div
+        style={{
+          width: "30%",
+        }}
+      >
+        <h3>DeviTrack</h3>
+        {/* <img
+          style={{
+            padding: "5px",
+          }}
+          src="https://picsum.photos/100/100"
+          alt=""
+        /> */}
       </div>
-      <div>
-        <div><h4>Change Language</h4></div>
+      <div
+        style={{
+          width: "30%",
+        }}>
+        <div>
+          <h4>Change Language</h4>
+        </div>
       </div>
-      <div>
+      <div
+        style={{
+          width: "30%",
+        }}>
         <h4>Event Name</h4>
       </div>
     </nav>
