ab045076afcdcc30936a04a0128be017df4d0c03
Gustavo <grodriguez@contextglobal.com>
2022-08-18 17:16:40 -0400

    navigation between components in progress

--- src/App.js
+++ src/App.js
@@ -1,29 +1,47 @@
 import React from "react";
-import {PaymentElement} from '@stripe/react-stripe-js';
+import { Routes, Route } from "react-router";
 import "./App.css";
-import { ContactInfo } from "./components/ContactInfo";
-import { Devices } from "./components/Devices";
 import { Navbar } from "./components/Navbar";
-import { PaymentInfo } from "./components/PaymentInfo";
+import { NavbarFixed } from "./components/NavbarFixed";
+import { EventSchedule } from "./page/EventSchedule";
+import { Home } from "./page/Home";
+import { MoreInfo } from "./page/MoreInfo";
+import { HowToReturnTheDevices } from "./page/moreInfo/HowToReturnTheDevices";
+import { HowToUseTheReceiver } from "./page/moreInfo/HowToUseTheReceiver";
+import { RequestSupportDuringTheEvent } from "./page/moreInfo/RequestSupportDuringTheEvent";
+import { MyProfile } from "./page/MyProfile";
+import { QRCodeConfirmation } from "./page/QRCodeConfirmation";
+import { RequestDevices } from "./page/RequestDevices";
 
 function App() {
-  const { amountToDeposit } = Devices;
-
-  const handleOnSubmit = (event) => {
-    event.preventDefault();
-    console.log("submitted");
-    console.log(amountToDeposit);
-  };
-
   return (
     <div className="App">
       <Navbar />
-      <form onSubmit={handleOnSubmit}>
-        <Devices amountToDeposit={amountToDeposit} />
-        <ContactInfo />
-        <PaymentInfo />
-        <button style={{ margin: 'auto'}} type="submit">SUBMIT AND REQUEST DEVICES</button>
-      </form>
+
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/confirmation" element={<QRCodeConfirmation />} />
+        <Route exact path="/request_devices" element={<RequestDevices />} />
+        <Route exact path="more_info" element={<MoreInfo />} />
+        <Route
+          exact
+          path="/how_to_use_the_receiver"
+          element={<HowToReturnTheDevices />}
+        />
+        <Route
+          exact
+          path="/how_to_return_the_devices"
+          element={<HowToUseTheReceiver />}
+        />
+        <Route
+          exact
+          path="/request_support_during_event"
+          element={<RequestSupportDuringTheEvent />}
+        />
+        <Route exact path="/event_schedule" element={<EventSchedule />} />
+        <Route exact path="/my_profile" element={<MyProfile />} />
+      </Routes>
+      <NavbarFixed />
     </div>
   );
 }
