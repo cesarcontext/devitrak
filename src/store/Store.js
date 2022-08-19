f97c67c14340e1ac7043cf2a7e589b5d7c4f801b
Gustavo <grodriguez@contextglobal.com>
2022-08-19 17:48:53 -0400

    payment reducer imported

--- src/store/Store.js
+++ src/store/Store.js
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
 import contactInfoSlice from './slices/contactInfoSlice'
 import deviceSlice from './slices/deviceSlice'
+import paymentInfoSlice from './slices/paymentInfoSlice'
 import uiSlice from './slices/uiSlice'
 
 export const store = configureStore({
   reducer: {
     device: deviceSlice,
     ui: uiSlice,
-    contactInfo: contactInfoSlice
+    contactInfo: contactInfoSlice,
+    paymentInfo: paymentInfoSlice
   },
 })
