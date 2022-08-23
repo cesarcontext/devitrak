1ed6b4c52816e1777e6f09686f69a33c85626455
Gustavo <grodriguez@contextglobal.com>
2022-08-23 17:02:49 -0400

    some variables and functions added in component to clean up payment component

--- src/hooks/useDeviceCount.js
+++ src/hooks/useDeviceCount.js
@@ -1,21 +1,54 @@
 import { useDispatch, useSelector } from "react-redux";
+import { increment, decrement, reset } from "../store/slices/deviceSlice";
+
 
 export const useDeviceCount = () => {
 
   const device = useSelector((state) => state.device.value);
+  const dispatch = useDispatch()
 
-  const deviceArray = new Array(device)
+  localStorage.setItem("device", device);
+
+  // const deviceArray = new Array(device)
+
+  const handleIncreaseDevice = (event) => {
+    event.preventDefault();
+    dispatch(increment());
+  };
+
+  const handleDecreaseDevice = (event) => {
+    event.preventDefault();
+    dispatch(decrement());
+
+    if(device <= 0 ) {
+      return dispatch( reset() )
+    }
+
+  };
+  
+  const handleResetDevice = (event) => {
+    event.preventDefault();
+    dispatch(reset());
+  };
 
   const renderSpotToDevice = () => {
 
   }
 
+  const amountToDeposit = device * 200
+
 
   return {
     //* Propiedades
     device,
-    deviceArray,
+    // deviceArray,
+    amountToDeposit,
+
+
     //* Métodos
-    renderSpotToDevice
+    renderSpotToDevice,
+    handleIncreaseDevice,
+    handleDecreaseDevice,
+    handleResetDevice,
   };
 };
