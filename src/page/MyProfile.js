d75e19293237ce5a709c59070b44e6aec1456e1d
Gustavo <grodriguez@contextglobal.com>
2022-08-25 21:29:21 -0400

    componenet cleaned up, accordion, alert, contac inof and payment info imported from different components

--- src/page/MyProfile.js
+++ src/page/MyProfile.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Accordion } from "../components/Accordion";
+import { ContactInfoProfile } from "../components/ContactInfoProfile";
+import { PaymentInfoProfile } from "../components/PaymentInfoProfile";
+import { ReturnDeviceAlert } from "../components/ReturnDeviceAlert";
 import { useContactInfoStore } from "../hooks/useContactInfoStore";
 import { useDeviceCount } from "../hooks/useDeviceCountStore";
 
 export const MyProfile = () => {
-
-const { deviceRented, deviceSelected } = useDeviceCount()
-const { userParseStored } = useContactInfoStore()
+  const { deviceRented, deviceSelected } = useDeviceCount();
+  const { userParseStored } = useContactInfoStore();
 
   return (
     <div
@@ -42,215 +45,40 @@ const { userParseStored } = useContactInfoStore()
               borderRadius: "15px",
             }}
           >
-            <div>
-              <div
-                className=""
-                style={{
-                  display: "flex",
-                  justifyContent: "flex-start",
-                  alignItem: "center",
-                }}
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  className="bi bi-pencil"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
-                </svg>
-                <h5> EDIT </h5>
-              </div>
-            </div>
-            <div>
-              <h5>Your conntact infomartion</h5>
-            </div>
-            {userParseStored?.map((item) => {
-              return (
-                <div>
-                  <div
-                    style={{
-                      display: "flex",
-                      justifyContent: "center",
-                    }}
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      fill="currentColor"
-                      className="bi bi-person"
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
-                    </svg>
-                    <div>
-                      <span> {item.name}</span>
-                      <span> {item.lastName}</span>
-                    </div>
-                  </div>
-                  <div>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      fill="currentColor"
-                      className="bi bi-envelope"
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z" />
-                    </svg>
-                    <span> {item.email}</span>
-                  </div>
-                  <div>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      fill="currentColor"
-                      className="bi bi-phone"
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h6zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H5z" />
-                      <path d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
-                    </svg>
-                    <span> {item.phoneNumber}</span>
-                  </div>
-                </div>
-              );
-            })}
-            <hr style={{ width: "0%" }} />
-            <div>
-              <h5>Your payment Informacion</h5>
-            </div>
             <div
-              style={{
-                margin: "15px auto",
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "right",
-                alignItems: "center",
-                width: "60%",
-                textAlign: "left",
-              }}
-            >
-              {userParseStored.map((item) => {
-                return <>
-                <div>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  className="bi bi-credit-card"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm2-1a1 1 0 0 0-1 1v1h14V4a1 1 0 0 0-1-1H2zm13 4H1v5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V7z" />
-                  <path d="M2 10a1 1 0 0 1 1-1h1a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1v-1z" />
-                </svg>
-                <span> {item.cardNumber}</span>
-              </div>
-              <div>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  className="bi bi-calendar-date"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M6.445 11.688V6.354h-.633A12.6 12.6 0 0 0 4.5 7.16v.695c.375-.257.969-.62 1.258-.777h.012v4.61h.675zm1.188-1.305c.047.64.594 1.406 1.703 1.406 1.258 0 2-1.066 2-2.871 0-1.934-.781-2.668-1.953-2.668-.926 0-1.797.672-1.797 1.809 0 1.16.824 1.77 1.676 1.77.746 0 1.23-.376 1.383-.79h.027c-.004 1.316-.461 2.164-1.305 2.164-.664 0-1.008-.45-1.05-.82h-.684zm2.953-2.317c0 .696-.559 1.18-1.184 1.18-.601 0-1.144-.383-1.144-1.2 0-.823.582-1.21 1.168-1.21.633 0 1.16.398 1.16 1.23z" />
-                  <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
-                </svg>
-                <span> {item.mm} / {item.yy}</span>
-              </div>
-              <div>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  className="bi bi-geo-alt"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z" />
-                  <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
-                </svg>
-                <span> {item.zip}</span>
-              </div></>;
-              })}
-              
-            </div>
-          </div>
-        </div>
-        <div style={{ width: "20%", margin: "auto" }}>
-          {deviceSelected > 0 ? (
-            <div style={{ width: "100%", margin: "auto" }}>
-              <div
-                style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  margin: "25px",
-                }}
-                className="alert alert-danger d-flex align-items-center"
-                role="alert"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="50"
-                  height="35"
-                  fill="currentColor"
-                  className="bi bi-exclamation-triangle"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M7.938 2.016A.13.13 0 0 1 8.002 2a.13.13 0 0 1 .063.016.146.146 0 0 1 .054.057l6.857 11.667c.036.06.035.124.002.183a.163.163 0 0 1-.054.06.116.116 0 0 1-.066.017H1.146a.115.115 0 0 1-.066-.017.163.163 0 0 1-.054-.06.176.176 0 0 1 .002-.183L7.884 2.073a.147.147 0 0 1 .054-.057zm1.044-.45a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566z" />
-                  <path d="M7.002 12a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 5.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995z" />
-                </svg>
-                <div>
-                  <h4>
-                    You need to return {deviceSelected}{" "}
-                    {deviceSelected > 1 ? "devices" : "device"}
-                  </h4>
-                  <span>
-                    You have 3 days remaining. <br />
-                    Devices not returned within 3 days will be charged to your
-                    credit card on file.
-                  </span>
-                </div>
-              </div>
-            </div>
-          ) : (
-            <div
-              className="alert alert-success d-flex align-items-center"
-              role="alert"
+              className=""
               style={{
                 display: "flex",
-                flexDirection: "column",
-                margin: "25px",
+                justifyContent: "flex-end",
+                alignItem: "center",
               }}
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="50"
-                height="35"
+                width="16"
+                height="16"
                 fill="currentColor"
-                className="bi bi-check-circle-fill"
+                className="bi bi-pencil"
                 viewBox="0 0 16 16"
               >
-                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+                <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
               </svg>
-              <div>
-                <h4>
-                  You have {deviceSelected} pending{" "}
-                  {deviceSelected > 1 ? "devices" : "device"}
-                </h4>
-                <span>You have returned all your devices.</span>
-              </div>
+              <h5> EDIT </h5>
             </div>
-          )}
+            <div>
+              <h5>Your conntact infomartion</h5>
+            </div>
+            <div>
+              <ContactInfoProfile />
+            </div>
+            <hr style={{ width: "0%" }} />
+            <div>
+              <h5>Your payment Informacion</h5>
+            </div>
+            <PaymentInfoProfile />
+          </div>
         </div>
+        <ReturnDeviceAlert />
         <div>
           <button
             style={{
@@ -277,72 +105,7 @@ const { userParseStored } = useContactInfoStore()
             <h3>Your devices</h3>
             <span>All the devices with checkmarks are now returned</span>
           </div>
-          <div
-            style={{ width: "50%", margin: "auto", border: "solid 1px #fff" }}
-          >
-            <div className="accordion" id="accordionExample">
-              <div className="accordion-item">
-                <h2 className="accordion-header" id="headingOne">
-                  <button
-                    className="accordion-button"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#collapseOne"
-                    aria-expanded="true"
-                    aria-controls="collapseOne"
-                  >
-                    <h3>PENDING DEVICES</h3>
-                  </button>
-                </h2>
-                <div
-                  id="collapseOne"
-                  className="accordion-collapse collapse show"
-                  aria-labelledby="headingOne"
-                  data-bs-parent="#accordionExample"
-                >
-                  <div className="accordion-body">
-                    {deviceRented?.map(( device, index ) => {
-                      return (
-                        <div key={index}>{device+1}<input disabled /></div>
-                      )
-                    })}
-                  </div>
-                </div>
-              </div>
-              <div className="accordion-item">
-                <h2 className="accordion-header" id="headingTwo">
-                  <button
-                    className="accordion-button collapsed"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#collapseTwo"
-                    aria-expanded="false"
-                    aria-controls="collapseTwo"
-                  >
-                    <h3>RETURNED DEVICES</h3>
-                  </button>
-                </h2>
-                <div
-                  id="collapseTwo"
-                  className="accordion-collapse collapse"
-                  aria-labelledby="headingTwo"
-                  data-bs-parent="#accordionExample"
-                >
-                  <div className="accordion-body">
-                    <li>1</li>
-                    <li>2</li>
-                    <li>3</li>
-                    <li>4</li>
-                    <li>5</li>
-                    <li>6</li>
-                    <li>7</li>
-                    <li>8</li>
-                    <li>9</li>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+          <Accordion />
         </div>
       </div>
     </div>
