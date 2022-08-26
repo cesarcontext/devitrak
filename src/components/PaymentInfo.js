87c30d720c77db78f9a7532eeac17f36c18a2211
Gustavo <grodriguez@contextglobal.com>
2022-08-25 21:07:18 -0400

    component being improved and clean it up

--- src/components/PaymentInfo.js
+++ src/components/PaymentInfo.js
@@ -4,10 +4,12 @@ import { ConfirmationModal } from "../ui/ConfirmationModal";
 import { useContactInfoStore } from "../hooks/useContactInfoStore";
 import { useDeviceCount } from "../hooks/useDeviceCountStore";
 import Swal from "sweetalert2";
-
+import { usePaymentStore } from "../hooks/usePaymentStore";
 
 export const PaymentInfo = () => {
   const { openModal } = useUiStore();
+  const { startSavingContactInfo } = useContactInfoStore();
+  const { startSavingPaymentInfo } = usePaymentStore();
 
   const {
     amountToDeposit,
@@ -18,7 +20,6 @@ export const PaymentInfo = () => {
   } = useDeviceCount();
 
   //
-  const { startSavingContactInfo } = useContactInfoStore();
 
   const initalFormValues = {
     groupName: "",
@@ -26,6 +27,9 @@ export const PaymentInfo = () => {
     lastName: "",
     email: "",
     phoneNumber: "",
+  };
+
+  const initalPaymentFormValues = {
     cardName: "",
     cardNumber: "",
     mm: "",
@@ -36,6 +40,9 @@ export const PaymentInfo = () => {
   };
 
   const [formValues, setFormValues] = useState(initalFormValues);
+  const [paymentFormValues, setPaymentFormValues] = useState(
+    initalPaymentFormValues
+  );
 
   const [formSubmitted, setFormSubmitted] = useState(false);
 
@@ -46,7 +53,16 @@ export const PaymentInfo = () => {
     });
   };
 
+
+  const onInputChangePayment = ({ target }) => {
+    setPaymentFormValues({
+      ...paymentFormValues,
+      [target.name]: target.value,
+    });
+  };
+
   const validationGroupName = useMemo(() => {
+
     return formValues.groupName.length > 2 ? "" : "is-invalid";
   }, [formValues.groupName, formSubmitted]);
 
@@ -74,39 +90,39 @@ export const PaymentInfo = () => {
   }, [formValues.phoneNumber, formSubmitted]);
 
   const validationCardName = useMemo(() => {
-    return formValues.cardName.length > 2 ? "" : "is-invalid";
-  }, [formValues.cardName, formSubmitted]);
+    return paymentFormValues.cardName.length > 2 ? "" : "is-invalid";
+  }, [paymentFormValues.cardName, formSubmitted]);
 
   const validationCardNumber = useMemo(() => {
-    return formValues.cardNumber.length > 12 ? "" : "is-invalid";
-  }, [formValues.cardNumber, formSubmitted]);
+    return paymentFormValues.cardNumber.length > 12 ? "" : "is-invalid";
+  }, [paymentFormValues.cardNumber, formSubmitted]);
 
   const validationExpirationDateMM = useMemo(() => {
     if (
-      formValues.mm < new Date().getMonth() + 1 &&
-      formValues.yy <= new Date().getFullYear().toString()
+      paymentFormValues.mm < new Date().getMonth() + 1 &&
+      paymentFormValues.yy <= new Date().getFullYear().toString()
     ) {
       return "is-invalid";
     }
-  }, [formValues.mm, formValues.yy, formSubmitted]);
+  }, [paymentFormValues.mm, paymentFormValues.yy, formSubmitted]);
 
   const validationExpirationDateYY = useMemo(() => {
-    if (formValues.yy.valueOf() < new Date().getFullYear().toString()) {
+    if (paymentFormValues.yy.valueOf() < new Date().getFullYear().toString()) {
       return "is-invalid";
     }
-  }, [formValues.yy, formSubmitted]);
+  }, [paymentFormValues.yy, formSubmitted]);
 
   const validationCvv = useMemo(() => {
-    return formValues.cvv.length > 2 ? "" : "is-invalid";
-  }, [formValues.cvv, formSubmitted]);
+    return paymentFormValues.cvv.length > 2 ? "" : "is-invalid";
+  }, [paymentFormValues.cvv, formSubmitted]);
 
   const validationZip = useMemo(() => {
-    return formValues.zip.length > 0 ? "" : "is-invalid";
-  }, [formValues.zip, formSubmitted]);
+    return paymentFormValues.zip.length > 0 ? "" : "is-invalid";
+  }, [paymentFormValues.zip, formSubmitted]);
 
   const validationCountry = useMemo(() => {
-    return formValues.country.length > 0 ? "" : "is-invalid";
-  }, [formValues.country, formSubmitted]);
+    return paymentFormValues.country.length > 0 ? "" : "is-invalid";
+  }, [paymentFormValues.country, formSubmitted]);
 
   const handleOnSubmit = async (event) => {
     event.preventDefault();
@@ -120,53 +136,112 @@ export const PaymentInfo = () => {
       });
     }
     if (validationName === "is-invalid") {
-      return Swal.fire("", "Name must be provided", "error");
+      return Swal.fire({
+        title: "",
+        text: "Group Name must be provided",
+        icon: "error",
+        confirmButtonColor: "rgb(30, 115, 190)",
+      });
     }
     if (validationLastName === "is-invalid") {
-      return Swal.fire("", "Last name must be provided", "error");
+      return Swal.fire({
+        title: "",
+        text: "Group Name must be provided",
+        icon: "error",
+        confirmButtonColor: "rgb(30, 115, 190)",
+      });
     }
     if (validationEmail === "is-invalid") {
-      return Swal.fire("", "Email is invalid", "error");
+      return Swal.fire({
+        title: "",
+        text: "Group Name must be provided",
+        icon: "error",
+        confirmButtonColor: "rgb(30, 115, 190)",
+      });
     }
     if (validationPhoneNumber === "is-invalid") {
-      return Swal.fire("", "Phone number must be provided", "error");
+      return Swal.fire({
+        title: "",
+        text: "Group Name must be provided",
+        icon: "error",
+        confirmButtonColor: "rgb(30, 115, 190)",
+      });
     }
     if (validationExpirationDateMM === "is-invalid") {
-      return Swal.fire("", "Please provide a valid expiration date", "error");
+      return Swal.fire({
+        title: "",
+        text: "Group Name must be provided",
+        icon: "error",
+        confirmButtonColor: "rgb(30, 115, 190)",
+      });
     }
     if (validationExpirationDateYY === "is-invalid") {
-      return Swal.fire("", "Please provide a valid expiration date", "error");
+      return Swal.fire({
+        title: "",
+        text: "Group Name must be provided",
+        icon: "error",
+        confirmButtonColor: "rgb(30, 115, 190)",
+      });
     }
     if (validationCardName === "is-invalid") {
-      return Swal.fire("", "Card name must be provided", "error");
+      return Swal.fire({
+        title: "",
+        text: "Group Name must be provided",
+        icon: "error",
+        confirmButtonColor: "rgb(30, 115, 190)",
+      });
     }
     if (validationCardNumber === "is-invalid") {
-      return Swal.fire("", "Card number must be provided", "error");
+      return Swal.fire({
+        title: "",
+        text: "Group Name must be provided",
+        icon: "error",
+        confirmButtonColor: "rgb(30, 115, 190)",
+      });
     }
     if (validationCvv === "is-invalid") {
-      return Swal.fire("", "Cvv must be provided", "error");
+      return Swal.fire({
+        title: "",
+        text: "Group Name must be provided",
+        icon: "error",
+        confirmButtonColor: "rgb(30, 115, 190)",
+      });
     }
     if (validationZip === "is-invalid") {
-      return Swal.fire("", "Zip must be provided", "error");
+      return Swal.fire({
+        title: "",
+        text: "Group Name must be provided",
+        icon: "error",
+        confirmButtonColor: "rgb(30, 115, 190)",
+      });
     }
     if (validationCountry === "is-invalid") {
-      return Swal.fire("", "Country name must be provided", "error");
+      return Swal.fire({
+        title: "",
+        text: "Group Name must be provided",
+        icon: "error",
+        confirmButtonColor: "rgb(30, 115, 190)",
+      });
     }
 
-    if (formValues.cvv.valueOf().length < 0) {
-      return;
+    if (validationCvv === 'is-invalid') {
+      return Swal.fire({
+        title: "",
+        text: "Group Name must be provided",
+        icon: "error",
+        confirmButtonColor: "rgb(30, 115, 190)",
+      });
     }
 
     setFormSubmitted(true);
     await startSavingContactInfo(formValues);
+    await startSavingPaymentInfo(paymentFormValues);
     openModal();
   };
 
-  
-
   return (
     <>
-      <div style={{ height: "100%", marginBottom: "18vh" }}>
+      <div className="container">
         <form onSubmit={handleOnSubmit}>
           <div
             className="container"
@@ -235,6 +310,7 @@ export const PaymentInfo = () => {
                           <div className="col-md-10 m-4 d-flex align-items-center">
                             <div className="form-outline datepicker w-100">
                               <input
+                              
                                 type="text"
                                 className={`form-control ${validationGroupName}  form-control-lg`}
                                 id="groupName"
@@ -333,9 +409,9 @@ export const PaymentInfo = () => {
                                 type="text"
                                 className="form-control  form-control-lg"
                                 placeholder="Card name"
-                                onChange={onInputCHange}
+                                onChange={onInputChangePayment}
                                 name="cardName"
-                                value={formValues.cardName}
+                                value={paymentFormValues.cardName}
                                 minLength={3}
                               />
                             </div>
@@ -346,9 +422,9 @@ export const PaymentInfo = () => {
                                 type="text"
                                 className="form-control form-control-lg cardNumber"
                                 placeholder="Card number"
-                                onChange={onInputCHange}
+                                onChange={onInputChangePayment}
                                 name="cardNumber"
-                                value={formValues.cardNumber}
+                                value={paymentFormValues.cardNumber}
                                 maxLength={19}
                                 minLength={13}
                               />
@@ -358,12 +434,13 @@ export const PaymentInfo = () => {
                             <div className="col-md-3 m-4">
                               <div className="form-outline">
                                 <input
+                                
                                   type="number"
                                   className={`form-control ${validationExpirationDateMM}  form-control-lg`}
                                   placeholder="MM"
-                                  onChange={onInputCHange}
+                                  onChange={onInputChangePayment}
                                   name="mm"
-                                  value={formValues.mm}
+                                  value={paymentFormValues.mm}
                                   maxLength={2}
                                   minLength={2}
                                   min={1}
@@ -377,9 +454,9 @@ export const PaymentInfo = () => {
                                   type="number"
                                   className={`form-control ${validationExpirationDateYY}  form-control-lg`}
                                   placeholder="YYYY"
-                                  onChange={onInputCHange}
+                                  onChange={onInputChangePayment}
                                   name="yy"
-                                  value={formValues.yy}
+                                  value={paymentFormValues.yy}
                                   maxLength={4}
                                   minLength={4}
                                   min={new Date().getFullYear()}
@@ -392,9 +469,9 @@ export const PaymentInfo = () => {
                                   type="text"
                                   className="form-control  form-control-lg"
                                   placeholder="CVV"
-                                  onChange={onInputCHange}
+                                  onChange={onInputChangePayment}
                                   name="cvv"
-                                  value={formValues.cvv}
+                                  value={paymentFormValues.cvv}
                                   maxLength={4}
                                   minLength={3}
                                 />
@@ -406,12 +483,13 @@ export const PaymentInfo = () => {
                           <div className="col-md-10 m-4">
                             <div className="form-outline">
                               <input
+                              id="zip"
                                 type="text"
                                 className={`form-control ${validationZip}  form-control-lg`}
                                 placeholder="Zip"
-                                onChange={onInputCHange}
+                                onChange={onInputChangePayment}
                                 name="zip"
-                                value={formValues.zip}
+                                value={paymentFormValues.zip}
                                 minLength={4}
                               />
                             </div>
@@ -422,9 +500,9 @@ export const PaymentInfo = () => {
                                 type="text"
                                 className={`form-control ${validationCountry}  form-control-lg`}
                                 placeholder="Country"
-                                onChange={onInputCHange}
+                                onChange={onInputChangePayment}
                                 name="country"
-                                value={formValues.country}
+                                value={paymentFormValues.country}
                                 minLength={3}
                               />
                             </div>
