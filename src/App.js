80f2f0026017d171fa19a35da807f8678e016304
Gustavo <grodriguez@contextglobal.com>
2022-08-25 21:32:14 -0400

    routes redefined

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router";
 import "./App.css";
 import { Navbar } from "./components/Navbar";
-import { NavbarFixed } from "./components/NavbarFixed";
+import { NavbarBottom } from "./components/NavbarBottom";
 import { EventScheduled } from "./page/EventScheduled";
 import { Home } from "./page/Home";
 import { MoreInfo } from "./page/MoreInfo";
@@ -18,32 +18,35 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-
+              
       <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/confirmation" element={<QRCodeConfirmation />} />
-        <Route exact path="/request_devices" element={<RequestDevices />} />
-        <Route exact path="/more_devices" element={ <MoreDevices />} />
-        <Route exact path="more_info" element={<MoreInfo />} />
-        <Route
-          exact
-          path="/how_to_return_the_devices"
-          element={<HowToReturnTheDevices />}
-        />
-        <Route
-          exact
-          path="/how_to_use_the_receiver" //how_to_return_the_devices
-          element={<HowToUseTheReceiver />}
-        />
-        <Route
-          exact
-          path="/request_support_during_event"
-          element={<RequestSupportDuringTheEvent />}
-        />
-        <Route exact path="/event_schedule" element={<EventScheduled />} />
-        <Route exact path="/my_profile" element={<MyProfile />} />
-      </Routes>
-      <NavbarFixed />
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/confirmation" element={<QRCodeConfirmation />} />
+          <Route exact path="/request_devices" element={<RequestDevices />} />
+          <Route exact path="/more_devices" element={<MoreDevices />} />
+          <Route exact path="more_info" element={<MoreInfo />} />
+          <Route
+            exact
+            path="/how_to_return_the_devices"
+            element={<HowToReturnTheDevices />}
+          />
+          <Route
+            exact
+            path="/how_to_use_the_receiver" //how_to_return_the_devices
+            element={<HowToUseTheReceiver />}
+          />
+          <Route
+            exact
+            path="/request_support_during_event"
+            element={<RequestSupportDuringTheEvent />}
+          />
+          <Route exact path="/event_schedule" element={<EventScheduled />} />
+          <Route exact path="/my_profile" element={<MyProfile />} />
+        </Routes>
+
+      <div className="App-footer">
+        <NavbarBottom />
+      </div>
     </div>
   );
 }
