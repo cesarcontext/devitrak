bd8b2b373fd06e4bb7d6a157ff0b91ab0d214fad
Gustavo <grodriguez@contextglobal.com>
2022-08-30 17:37:02 -0400

    id added

--- src/components/userInfoSaved.js
+++ src/components/userInfoSaved.js
@@ -2,14 +2,16 @@ import React from "react";
 
 export const userInfoSaved = ({ user }) => {
 
-  const {_id, groupName, name, lastName, email, phoneNumber,cardName, cardNumber, mm, yy, cvv, zip, country} = user
+  const { groupName, name, lastName, email, phoneNumber } = user
+  const _id = new Date().getDate()
 
   return (
-    <div>
+    <div key={ _id }>
       <div>{groupName}</div>
       <div>{name}</div>
       <div>{lastName}</div>
       <div>{email}</div>
+      <div>{phoneNumber}</div>
     </div>
   );
 };
