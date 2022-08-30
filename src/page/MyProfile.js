c22baa6ca8db0a7765f2d3612e0f43345657108a
Gustavo <grodriguez@contextglobal.com>
2022-08-30 17:39:22 -0400

    contac info => work on be editable and then push into localStorage

--- src/page/MyProfile.js
+++ src/page/MyProfile.js
@@ -1,14 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { Accordion } from "../components/Accordion";
 import { ContactInfoProfile } from "../components/ContactInfoProfile";
 import { PaymentInfoProfile } from "../components/PaymentInfoProfile";
 import { ReturnDeviceAlert } from "../components/ReturnDeviceAlert";
 import { useContactInfoStore } from "../hooks/useContactInfoStore";
-import { useDeviceCount } from "../hooks/useDeviceCountStore";
+
+const editContactFormValues = {
+  name: "",
+  lastName: "",
+  email: "",
+  phoneNumber: "",
+};
 
 export const MyProfile = () => {
-  const { deviceRented, deviceSelected } = useDeviceCount();
-  const { userParseStored } = useContactInfoStore();
+  const { userParseStored, startUpdatingContactInfo, startSavingContactInfo } = useContactInfoStore();
+
+  const [showInfo, setShowInfo] = useState(false);
+  const [editContactInfo, setEditContactInfo] = useState(editContactFormValues);
+
+  const onInputCHange = ({ target }) => {
+    setEditContactInfo({
+      ...editContactInfo,
+      [target.name]: target.value,
+    });
+  };
+
+  const handleButtonState = () => {
+    setShowInfo(!showInfo);
+  };
+
+  const handleEditContactInfo = async (event) => {
+    event.preventDefault();
+
+   await startSavingContactInfo(editContactInfo)
+  };
+
+  console.log({editContactInfo})
+
 
   return (
     <div
@@ -53,24 +81,115 @@ export const MyProfile = () => {
                 alignItem: "center",
               }}
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                className="bi bi-pencil"
-                viewBox="0 0 16 16"
-              >
-                <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
-              </svg>
-              <h5> EDIT </h5>
+              {showInfo !== true ? (
+                <button
+                  style={{
+                    width: "25%",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    borderRadius: "0px 15px ",
+                    borderRight: "transparent",
+                    borderTop: "transparent",
+                    border: "solid 1px #212529",
+                  }}
+                  onClick={handleButtonState}
+                >
+                  <h5>
+                    EDIT{" "}
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="15"
+                      height="15"
+                      fill="currentColor"
+                      className="bi bi-pencil"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
+                    </svg>
+                  </h5>
+                </button>
+              ) : (
+                <button
+                  onClick={(handleEditContactInfo, handleButtonState)}
+                  style={{
+                    width: "25%",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    borderRadius: "0px 15px ",
+                    borderRight: "transparent",
+                    borderTop: "transparent",
+                    border: "solid 1px #212529",
+                  }}
+                >
+                  <h5>
+                    SAVE{" "}
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="15"
+                      height="15"
+                      fill="currentColor"
+                      className="bi bi-save"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M2 1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H9.5a1 1 0 0 0-1 1v7.293l2.646-2.647a.5.5 0 0 1 .708.708l-3.5 3.5a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L7.5 9.293V2a2 2 0 0 1 2-2H14a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h2.5a.5.5 0 0 1 0 1H2z" />
+                    </svg>
+                  </h5>
+                </button>
+              )}
             </div>
             <div>
               <h5>Your conntact infomartion</h5>
             </div>
-            <div>
-              <ContactInfoProfile />
-            </div>
+            {showInfo !== true ? (
+              <div>
+                <ContactInfoProfile />
+              </div>
+            ) : (
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                }}
+              >
+                {userParseStored?.map((item) => {
+                  return (
+                    <>
+                      <input
+                        style={{ width: "50%" }}
+                        type="text"
+                        name="name"
+                        onChange={onInputCHange}
+                        placeholder={item.name}
+                      />
+                      <input
+                        style={{ width: "50%" }}
+                        type="text"
+                        name="lastName"
+                        onChange={onInputCHange}
+                        placeholder={item.lastName}
+                      />
+                      <input
+                        style={{ width: "50%" }}
+                        type="text"
+                        name="email"
+                        onChange={onInputCHange}
+                        placeholder={item.email}
+                      />
+                      <input
+                        style={{ width: "50%" }}
+                        type="number"
+                        name="phoneNumber"
+                        onChange={onInputCHange}
+                        placeholder={item.phoneNumber}
+                      />
+                    </>
+                  );
+                })}
+              </div>
+            )}
+
             <hr style={{ width: "0%" }} />
             <div>
               <h5>Your payment Informacion</h5>
