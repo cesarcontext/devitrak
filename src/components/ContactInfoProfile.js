8783edd3150fd123367d00248ebe9a552405eaec
Gustavo <grodriguez@contextglobal.com>
2022-09-01 17:24:53 -0400

    imported component removed

--- src/components/ContactInfoProfile.js
+++ src/components/ContactInfoProfile.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { useContactInfoStore } from '../hooks/useContactInfoStore';
-import { MyProfile } from '../page/MyProfile';
 
 export const ContactInfoProfile = () => {
 
