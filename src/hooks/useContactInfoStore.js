ca396a8e4f3dbafc9da1f989d37bb28a058c63b7
Gustavo <grodriguez@contextglobal.com>
2022-09-02 19:48:33 -0400

    function to save data in localStorage before fetch to database added, manage data and error response from server, modal reducers dispatched from hook

--- src/hooks/useContactInfoStore.js
+++ src/hooks/useContactInfoStore.js
@@ -1,33 +1,88 @@
-import { current } from "@reduxjs/toolkit";
 import { useDispatch, useSelector } from "react-redux";
+import { Link, Navigate } from "react-router-dom";
 import Swal from "sweetalert2";
 import devitrackApi from "../apis/devitrackApi";
 import {
   onAddNewContact,
   onUpdateContact,
 } from "../store/slices/contactInfoSlice";
+import { useUiStore } from "./useUiStore";
 
 export const useContactInfoStore = () => {
   const dispatch = useDispatch();
 
   const { users } = useSelector((state) => state.contactInfo);
+  const { openModal, closeModal } = useUiStore();
 
-  console.log("user", users);
+  const userInfo = {
+    groupName: users.groupName,
+    name: users.name,
+    lastName: users.lastName,
+    email: users.email,
+    phoneNumber: users.phoneNumber,
+  };
 
-  const startSavingContactInfo = async (userInfoSaved) => {
+  const startVerificationContactInfoBeforeSaveIt = (userInfoSaved) => {
+    dispatch(onAddNewContact({ ...userInfoSaved }));
+    localStorage.setItem("user", JSON.stringify(userInfoSaved));
+
+    openModal();
+  };
+
+  const startSavingContactInfo = async (user) => {
     try {
       const { data } = await devitrackApi.post("/auth/new", {
-        ...userInfoSaved,
+        ...user,
       });
       console.log({ data });
 
       localStorage.setItem("user", JSON.stringify(data.user));
       localStorage.setItem("id", JSON.stringify(data.user.id));
-      dispatch(onAddNewContact({ ...userInfoSaved, id: data.user.id }));
+
+      dispatch(onAddNewContact({ ...user, id: data.user.id }));
 
       console.log(data.user.id);
+
+      if (data) {
+        Swal.fire({
+          title: "Your account was created successfully",
+          width: 600,
+          padding: "3em",
+          text: `REFERENCE NUMBER: ${data.user.id}`,
+          icon:"success",
+          color: "#rgb(30, 115, 190)",
+          background: "#fff",
+          confirmButtonColor: "rgb(30, 115, 190)",
+          backdrop: `
+          rgb(30, 115, 190)
+            url("../image/logo.jpg")
+            left top
+            no-repeat
+          `,
+        });
+      }
     } catch (error) {
       console.log({ error });
+      <Link to="/">
+        {Swal.fire({
+          title: "Something went wrong",
+          width: 600,
+          padding: "3em",
+          text: error.response.data.msg,
+          icon: "error",
+          color: "rgb(30, 115, 190)",
+          background: "#fff",
+          confirmButtonColor: "rgb(30, 115, 190)",
+          backdrop: `
+          rgb(30, 115, 190)
+            url("../image/logo.jpg")
+            left top
+            no-repeat
+          `,
+        })}
+      </Link>;
+
+      closeModal();
     }
   };
 
@@ -51,6 +106,7 @@ export const useContactInfoStore = () => {
     userParseStored,
 
     //* Métodos
+    startVerificationContactInfoBeforeSaveIt,
     startSavingContactInfo,
     startUpdatingContactInfo,
   };
