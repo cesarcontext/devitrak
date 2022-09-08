ab23e4dcfd16eb48cd31d47cb50b5eaa6430dbb0
grodriguezcontextglobal <grodriguez@contextglobal.com>
2022-09-08 19:52:30 -0400

    link added to fetch data to server

--- src/apis/devitrackApi.js
+++ src/apis/devitrackApi.js
@@ -1,10 +1,7 @@
 import axios from 'axios'
-import { getEnvVariables } from '../helper/getEnvVariables'
-
-const { REACT_APP_URL } = getEnvVariables()
 
 const devitrackApi = axios.create({
-    baseURL: REACT_APP_URL
+    baseURL: "http://localhost:4444/api"
 });
 
 //TODO: config interceptors
