649118c0c5b8ebbb5b39bc993eaa3d96d9b6cca8
grodriguezcontextglobal <grodriguez@contextglobal.com>
2022-09-14 18:46:57 -0400

    email user check reducer and new contact data stores in localStorage

--- src/hooks/useContactInfoStore.js
+++ src/hooks/useContactInfoStore.js
@@ -5,6 +5,7 @@ import devitrackApi from "../apis/devitrackApi";
 import {
   onAddNewContact,
   onUpdateContact,
+  onCheckContact,
 } from "../store/slices/contactInfoSlice";
 import { useUiStore } from "./useUiStore";
 
@@ -23,34 +24,124 @@ export const useContactInfoStore = () => {
   };
 
   const startSavingContactInfo = async ({
-    groupName,
     name,
     lastName,
     email,
     phoneNumber,
   }) => {
-    console.log({ groupName, name, lastName, email, phoneNumber });
     try {
       const { data } = await devitrackApi.post("/auth/new", {
-        groupName,
         name,
         lastName,
         email,
         phoneNumber,
       });
-      console.log({ data });
+
+      console.log({data})
       localStorage.setItem("user", JSON.stringify({ ...data }));
       localStorage.setItem("uid", JSON.stringify(data.uid));
       localStorage.setItem("token", JSON.stringify(data.token));
 
       dispatch(
         onAddNewContact({
+          name: data.name,
+          lastName: data.lastName,
+          email: data.email,
+          phoneNumber: data.phoneNumber,
+          id: data.uid,
+        })
+      );
+
+      // if (data) {
+      //   Swal.fire({
+      //     title: "Your account was created successfully",
+      //     width: 600,
+      //     padding: "3em",
+      //     text: `REFERENCE NUMBER: ${data.uid}`,
+      //     icon: "success",
+      //     color: "#rgb(30, 115, 190)",
+      //     background: "#fff",
+      //     confirmButtonColor: "rgb(30, 115, 190)",
+      //     backdrop: `
+      //     rgb(30, 115, 190)
+      //       url("../image/logo.jpg")
+      //       left top
+      //       no-repeat
+      //     `,
+      //   });
+
+      //   return data;
+      // }
+    } catch (error) {
+      console.log({ error });
+
+      Swal.fire({
+        title: "Something went wrong",
+        width: 600,
+        padding: "3em",
+        text: error.message,
+        icon: "error",
+        color: "rgb(30, 115, 190)",
+        background: "#fff",
+        confirmButtonColor: "rgb(30, 115, 190)",
+        backdrop: `
+          rgb(30, 115, 190)
+            url("../image/logo.jpg")
+            left top
+            no-repeat
+          `,
+      });
+
+      navigate("/");
+
+      // closeModal();
+    }
+  };
+
+  const checkingId = localStorage.getItem("uid");
+  const Id = JSON.parse(checkingId);
+
+  const startShowingData = () => {
+    try {
+      const { data } = devitrackApi.get(`/auth/${Id}`);
+
+      console.log("data updated", { data });
+
+      localStorage.setItem("user", JSON.stringify({ ...data }));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const startUpdatingContactInfo = async ({
+    groupName,
+    name,
+    lastName,
+    email,
+    phoneNumber,
+  }) => {
+    try {
+      const { data } = await devitrackApi.put(`/auth/${Id}`, {
+        groupName,
+        name,
+        lastName,
+        email,
+        phoneNumber,
+      });
+
+      localStorage.setItem("user", JSON.stringify(data));
+      localStorage.setItem("uid", JSON.stringify(data.uid));
+      localStorage.setItem("token", JSON.stringify(data.token));
+
+      dispatch(
+        onUpdateContact({
           groupName: data.groupName,
           name: data.name,
           lastName: data.lastName,
           email: data.email,
           phoneNumber: data.phoneNumber,
           id: data.uid,
+          status: data.ok,
         })
       );
 
@@ -75,75 +166,6 @@ export const useContactInfoStore = () => {
         return data;
       }
     } catch (error) {
-      console.log({ error });
-
-      Swal.fire({
-        title: "Something went wrong",
-        width: 600,
-        padding: "3em",
-        text: error.message,
-        icon: "error",
-        color: "rgb(30, 115, 190)",
-        background: "#fff",
-        confirmButtonColor: "rgb(30, 115, 190)",
-        backdrop: `
-          rgb(30, 115, 190)
-            url("../image/logo.jpg")
-            left top
-            no-repeat
-          `,
-      });
-
-      navigate("/");
-
-      closeModal();
-    }
-  };
-
-  const checkingId = localStorage.getItem("id");
-  const Id = JSON.parse(checkingId);
-
-  const startShowingData = () => {
-    try {
-      const { data } = devitrackApi.get(`/auth/${Id}`);
-
-      localStorage.setItem("user", JSON.stringify(data.user));
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const startUpdatingContactInfo = async ({
-    groupName,
-    name,
-    lastName,
-    email,
-    phoneNumber,
-  }) => {
-    try {
-      const { data } = devitrackApi.put(`/auth/${Id}`, {
-        groupName,
-        name,
-        lastName,
-        email,
-        phoneNumber,
-      });
-
-      dispatch(
-        onUpdateContact({
-          groupName: data.user.groupName,
-          name: data.user.name,
-          lastName: data.user.lastName,
-          email: data.user.email,
-          phoneNumber: data.user.phoneNumberid,
-        })
-      );
-
-      localStorage.setItem(
-        "user",
-        JSON.stringify({ ...data.user, id: data.user.id })
-      );
-    } catch (error) {
       console.log(error);
     }
   };
@@ -153,16 +175,55 @@ export const useContactInfoStore = () => {
       const { data } = await devitrackApi.post("/auth/", {
         userInfoEmailCheck,
       });
-      console.log({ data });
-      if (data) {
+
+      if (data.ok === true) {
         localStorage.setItem(
           "user",
           JSON.stringify({ ...data.user, status: data.ok, id: data.user.id })
         );
         localStorage.setItem("uid", JSON.stringify(data.user.id));
+        localStorage.setItem("token", JSON.stringify(data.token));
+
+        dispatch(
+          onCheckContact({
+            groupName: data.user.groupName,
+            name: data.user.name,
+            lastName: data.user.lastName,
+            email: data.user.email,
+            phoneNumber: data.user.phoneNumber,
+            id: data.user.uid,
+            status: data.ok,
+          })
+        );
+      } else {
+        localStorage.setItem(
+          "user",
+          JSON.stringify({
+            groupName: "",
+            name: "",
+            lastName: "",
+            email: "",
+            phoneNumber: "",
+            id: "",
+            status: "",
+            status: "",
+            id: "",
+          })
+        );
+        localStorage.setItem("uid", JSON.stringify(""));
+        dispatch(
+          onCheckContact({
+            groupName: "",
+            name: "",
+            lastName: "",
+            email: "",
+            phoneNumber: "",
+            id: "",
+            status: "",
+          })
+        );
+        localStorage.setItem("token", JSON.stringify(""));
       }
-      return;
-      
     } catch (error) {
       console.log(error);
     }
@@ -170,6 +231,7 @@ export const useContactInfoStore = () => {
 
   const checking = localStorage.getItem("user");
   const userParseStored = [JSON.parse(checking)];
+  
 
   return {
     //* Properties
