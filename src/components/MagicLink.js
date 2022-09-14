d0161d7573e90d4ff74beed11ea4cf8084528c90
grodriguezcontextglobal <grodriguez@contextglobal.com>
2022-09-14 18:45:03 -0400

    magic link function set up in a separte component

--- src/components/MagicLink.js
+++ src/components/MagicLink.js
@@ -1,34 +1,37 @@
-import React, { useState } from "react";
 import { useStytch } from "@stytch/stytch-react";
 import Swal from "sweetalert2";
 
-export const MagicLink = ({formValues}) => {
-    const client = useStytch();
+export const MagicLink = (magicLinkParam) => {
+  const client = useStytch();
+
+  const emailUserToPassToMagicLink = Object.values(magicLinkParam )
+
+  let emailToPass;
   
-    const handleLogin = async (formValues) => {
-        formValues.preventDefault();
-        formValues.stopPropagation();
-    
-        await client.magicLinks.email.loginOrCreate(formValues.email);
+  const emailString = emailUserToPassToMagicLink.map((item ) => emailToPass = item)
 
-        Swal.fire({
-          title: `An email has been sent to ${ formValues.email }`,
-          confirmButtonColor: "rgb(30, 115, 190)",
-          showClass: {
-            popup: "animate__animated animate__fadeInDown",
-          },
-          hideClass: {
-            popup: "animate__animated animate__fadeOutUp",
-          },
-        });
-      };
+  console.log("first", emailToPass )
 
+  const handleLogin = async () => {
+    await client.magicLinks.email.loginOrCreate(emailToPass);
+
+    Swal.fire({
+      title: `An email has been sent to ${emailString}`,
+      confirmButtonColor: "rgb(30, 115, 190)",
+      showClass: {
+        popup: "animate__animated animate__fadeInDown",
+      },
+      hideClass: {
+        popup: "animate__animated animate__fadeOutUp",
+      },
+    });
+  };
 
   return (
-    <form onSubmit={handleLogin}>
-        <h4>Your email is already registered, please sign in</h4>
-    <span>{formValues.email}</span>
-    <button>Login</button>
-  </form>
-  )
-}
+    <div>
+      <h4>{emailToPass} is registered, please sign in</h4>
+
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+};
