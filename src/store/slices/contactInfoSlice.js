c0645c9a95ba06a3218a1c1d7ac02aa2931abacb
grodriguezcontextglobal <grodriguez@contextglobal.com>
2022-09-15 18:19:22 -0400

    errorMessage added to state

--- src/store/slices/contactInfoSlice.js
+++ src/store/slices/contactInfoSlice.js
@@ -1,22 +1,33 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const users = {
+const users = [
+  {
     id: "",
     groupName: "",
     name: "",
     email: "",
     phoneNumber: "",
-}
+    status: "",
+  }];
+
+  const errorMessage = undefined
 
 const contactInfoSlice = createSlice({
   name: "contactInfo",
   initialState: {
-    users: [ users ],
+    users,
+    errorMessage
   },
   reducers: {
-    onAddNewContact: (state, { payload }) => {
-      state.users.push( payload );
+    onCheckContact: (state, { payload }) => {
+      state.users.push(payload);
       state.users.shift();
+      state.errorMessage = undefined
+    },
+    onAddNewContact: (state, { payload }) => {
+      state.users.push(payload);
+      state.users.shift();
+      state.errorMessage = undefined
     },
     onUpdateContact: (state, { payload }) => {
       state.users = state.users.map((user) => {
@@ -32,4 +43,4 @@ const contactInfoSlice = createSlice({
 export default contactInfoSlice.reducer;
 
 // Action creators are generated for each case reducer function
-export const { onAddNewContact, onUpdateContact } = contactInfoSlice.actions;
+export const { onAddNewContact, onUpdateContact, onCheckContact } = contactInfoSlice.actions;
