ec99e7bc204ad59640b580f66742a1cc396a21e5
grodriguezcontextglobal <grodriguez@contextglobal.com>
2022-09-15 18:06:58 -0400

    login email and logout button added

--- src/components/NavbarBottom.js
+++ src/components/NavbarBottom.js
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import { Link } from "react-router-dom";
+import { useStytchSession, useStytch } from "@stytch/stytch-react";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import { useContactInfoStore } from "../hooks/useContactInfoStore";
 
 import "./navbar-bottom.css";
 
 export const NavbarBottom = () => {
+  const session = useStytchSession();
+  const client = useStytch();
+  const navigate = useNavigate();
+
+  const { users } = useContactInfoStore();
+
+  const user = session?.authentication_factors[0].email_factor.email_address;
+
+  const handleLogout = async () => {
+    
+    await client.session.revoke();
+    
+    Swal.fire({
+      title: `Your session is finished`,
+      confirmButtonColor: "rgb(30, 115, 190)",
+      showClass: {
+        popup: "animate__animated animate__fadeInDown",
+      },
+      hideClass: {
+        popup: "animate__animated animate__fadeOutUp",
+      },
+    });
+    localStorage.clear();
+    navigate("/");
+  };
+
   return (
     <div className="navbar-container">
       <nav className="navbar-fixed">
@@ -31,6 +61,21 @@ export const NavbarBottom = () => {
             <span>MY PROFILE</span>
           </div>
         </Link>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-around",
+          }}
+        >
+          {" "}
+            <div>{session ? user : users[0].email}</div>
+            <div>
+              {session || users[0].email ? (
+                <button onClick={handleLogout}>Logout</button>
+              ) : ""}
+            </div>
+        </div>
       </nav>
     </div>
   );
