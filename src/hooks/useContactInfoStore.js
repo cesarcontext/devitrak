42b01866283e268c2322ba5d66d59abd2ae38775
grodriguezcontextglobal <grodriguez@contextglobal.com>
2022-09-15 18:13:08 -0400

    fixing invalid JSON parse

--- src/hooks/useContactInfoStore.js
+++ src/hooks/useContactInfoStore.js
@@ -1,27 +1,17 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
-import devitrackApi from "../apis/devitrackApi";
+import { devitrackApi } from "../apis/devitrackApi";
 import {
   onAddNewContact,
   onUpdateContact,
   onCheckContact,
 } from "../store/slices/contactInfoSlice";
-import { useUiStore } from "./useUiStore";
 
 export const useContactInfoStore = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { users } = useSelector((state) => state.contactInfo);
-  const { openModal, closeModal } = useUiStore();
-
-  const startVerificationContactInfoBeforeSaveIt = (userInfoSaved) => {
-    console.log({ userInfoSaved });
-
-    dispatch(onAddNewContact({ ...userInfoSaved }));
-    localStorage.setItem("user", JSON.stringify(userInfoSaved));
-    openModal();
-  };
 
   const startSavingContactInfo = async ({
     name,
@@ -37,41 +27,21 @@ export const useContactInfoStore = () => {
         phoneNumber,
       });
 
-      console.log({data})
+      console.log({ data });
       localStorage.setItem("user", JSON.stringify({ ...data }));
       localStorage.setItem("uid", JSON.stringify(data.uid));
       localStorage.setItem("token", JSON.stringify(data.token));
+      localStorage.setItem("status", JSON.stringify(data.ok));
 
       dispatch(
         onAddNewContact({
           name: data.name,
           lastName: data.lastName,
           email: data.email,
-          phoneNumber: data.phoneNumber,
+          phoneNumber: data.phone,
           id: data.uid,
         })
       );
-
-      // if (data) {
-      //   Swal.fire({
-      //     title: "Your account was created successfully",
-      //     width: 600,
-      //     padding: "3em",
-      //     text: `REFERENCE NUMBER: ${data.uid}`,
-      //     icon: "success",
-      //     color: "#rgb(30, 115, 190)",
-      //     background: "#fff",
-      //     confirmButtonColor: "rgb(30, 115, 190)",
-      //     backdrop: `
-      //     rgb(30, 115, 190)
-      //       url("../image/logo.jpg")
-      //       left top
-      //       no-repeat
-      //     `,
-      //   });
-
-      //   return data;
-      // }
     } catch (error) {
       console.log({ error });
 
@@ -79,7 +49,7 @@ export const useContactInfoStore = () => {
         title: "Something went wrong",
         width: 600,
         padding: "3em",
-        text: error.message,
+        text: error.response.data.msg,
         icon: "error",
         color: "rgb(30, 115, 190)",
         background: "#fff",
@@ -99,7 +69,12 @@ export const useContactInfoStore = () => {
   };
 
   const checkingId = localStorage.getItem("uid");
+
+  if (checkingId === undefined) {
+    return (checking = "");
+  }
   const Id = JSON.parse(checkingId);
+  console.log({ Id });
 
   const startShowingData = () => {
     try {
@@ -114,7 +89,6 @@ export const useContactInfoStore = () => {
   };
 
   const startUpdatingContactInfo = async ({
-    groupName,
     name,
     lastName,
     email,
@@ -122,25 +96,27 @@ export const useContactInfoStore = () => {
   }) => {
     try {
       const { data } = await devitrackApi.put(`/auth/${Id}`, {
-        groupName,
         name,
         lastName,
         email,
         phoneNumber,
       });
 
-      localStorage.setItem("user", JSON.stringify(data));
+      console.log("data update contact", data);
+
+      localStorage.setItem("user", JSON.stringify({ ...data }));
       localStorage.setItem("uid", JSON.stringify(data.uid));
       localStorage.setItem("token", JSON.stringify(data.token));
+      localStorage.setItem("status", JSON.stringify(data.ok));
 
       dispatch(
         onUpdateContact({
-          groupName: data.groupName,
-          name: data.name,
-          lastName: data.lastName,
-          email: data.email,
-          phoneNumber: data.phoneNumber,
-          id: data.uid,
+          groupName: data.user.groupName,
+          name: data.user.name,
+          lastName: data.user.lastName,
+          email: data.user.email,
+          phoneNumber: data.user.phoneNumber,
+          id: data.user.id,
           status: data.ok,
         })
       );
@@ -150,7 +126,7 @@ export const useContactInfoStore = () => {
           title: "Your account was created successfully",
           width: 600,
           padding: "3em",
-          text: `REFERENCE NUMBER: ${data.uid}`,
+          text: `REFERENCE NUMBER: ${data.id}`,
           icon: "success",
           color: "#rgb(30, 115, 190)",
           background: "#fff",
@@ -231,16 +207,21 @@ export const useContactInfoStore = () => {
 
   const checking = localStorage.getItem("user");
   const userParseStored = [JSON.parse(checking)];
-  
+  const uidStored = localStorage.getItem("uid");
+  const uidParsed = [JSON.parse(uidStored)];
+  const tokenStored = localStorage.getItem("token");
+  const tokenParsed = [JSON.parse(tokenStored)];
 
   return {
     //* Properties
     users,
     userParseStored,
     Id,
+    uidParsed,
+    tokenParsed,
 
     //* Methods
-    startVerificationContactInfoBeforeSaveIt,
+    // startVerificationContactInfoBeforeSaveIt,
     startSavingContactInfo,
     startUpdatingContactInfo,
     startShowingData,
