5228f807ffc8340255fb2ff1189df028fc8ab515
grodriguezcontextglobal <grodriguez@contextglobal.com>
2022-09-28 09:08:31 -0400

    stripe elements and chekout form to be rendered and collect payment info

--- src/components/stripe/StripeCheckForm.js
+++ src/components/stripe/StripeCheckForm.js
@@ -4,12 +4,21 @@ import {
   useStripe,
   useElements,
 } from "@stripe/react-stripe-js";
+import { useContactInfoStore } from "../../hooks/useContactInfoStore"
 import "./checkoutStyles.css";
-import Swal from "sweetalert2";
 
 export const StripeCheckoutForm = () => {
   const stripe = useStripe();
   const elements = useElements();
+  const { userParseStored } = useContactInfoStore()
+
+  const userName = userParseStored.map( item => {
+    return item.name
+  })
+
+  const userEmail = userParseStored.map( item => {
+    return item.email
+  })
 
   const [message, setMessage] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
@@ -45,6 +54,34 @@ export const StripeCheckoutForm = () => {
     });
   }, [stripe]);
 
+  const billingDetails = {
+    name: userName[0],
+    email: userEmail[0],
+  };
+
+  const iFrameStyle = {
+    base: {
+      color: "#fff",
+      fontSize: "16px",
+      iconColor: "#fff",
+      "::placeholder": {
+        color: "#87bbfd"
+      }
+    },
+    invalid: {
+      iconColor: "#FFC7EE",
+      color: "#FFC7EE"
+    },
+    complete: {
+      iconColor: "#cbf4c9"
+    }
+  };
+
+  const paymentElementStyle = {
+    billing_details: billingDetails,
+    style: iFrameStyle,
+  }
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -55,7 +92,7 @@ export const StripeCheckoutForm = () => {
     }
 
     setIsLoading(true);
-
+    
     const { error } = await stripe.confirmPayment({
       elements,
       confirmParams: {
@@ -83,7 +120,7 @@ export const StripeCheckoutForm = () => {
       <div id="stripe-container" className="container">
         <div className="row justify-content-center align-items-center">
           <form id="payment-form" onSubmit={handleSubmit}>
-            <PaymentElement id="payment-element" />
+            <PaymentElement options={paymentElementStyle} id="payment-element" />
             <button disabled={isLoading || !stripe || !elements} id="submit">
               <span id="button-text">
                 {isLoading ? (
@@ -94,118 +131,10 @@ export const StripeCheckoutForm = () => {
               </span>
             </button>
             {/* Show any error or success messages */}
-            {message && <div id="payment-message">{Swal.fire({
-              title:"Something went wrong!",
-              text: message,
-              icon: "error",
-              confirmButtonColor: "#5469d4;"
-            }).then(()=> {
-              window.location.href="http://localhost:3000"
-            })}</div>}
+            {message && <div id="payment-message">{alert({message})}</div>}
           </form>
         </div>
       </div>
     </section>
   );
 };
-
-// import React, { useEffect, useState } from "react";
-// import {
-//   PaymentElement,
-//   useStripe,
-//   useElements,
-// } from "@stripe/react-stripe-js";
-// import { useStripeHook } from "../../hooks/useStripeHook";
-
-// export const StripeCheckoutForm = () => {
-//   const { PaymentIntent } = useStripeHook();
-//   const stripe = useStripe();
-//   const elements = useElements();
-
-//   const [message, setMessage] = useState(null);
-//   const [isLoading, setIsLoading] = useState(false);
-
-//   useEffect(() => {
-//     if (!stripe) {
-//       return;
-//     }
-
-//     const clientSecret = new URLSearchParams(window.location.search).get(
-//       "payment_intent_client_secret"
-//     );
-
-//     if (!clientSecret) {
-//       return;
-//     }
-
-//     stripe.retrievePaymentIntent(clientSecret).then(({ paymentIntent }) => {
-//       switch (paymentIntent.status) {
-//         case "succeeded":
-//           setMessage("Payment succeeded!");
-//           break;
-//         case "processing":
-//           setMessage("Your payment is processing.");
-//           break;
-//         case "requires_payment_method":
-//           setMessage("Your payment was not successful, please try again.");
-//           break;
-//         default:
-//           setMessage("Something went wrong.");
-//           break;
-//       }
-//     });
-//   }, [stripe]);
-
-//   const handleSubmit = async (event) => {
-//     event.preventDefault();
-
-//     if (!stripe || !elements) {
-//       // Stripe.js has not yet loaded.
-//       // Make sure to disable form submission until Stripe.js has loaded.
-//       return;
-//     }
-//     try {
-//       const amount = 80000;
-
-//       PaymentIntent(amount);
-
-//       setIsLoading(true);
-
-//       const { error } = await stripe.confirmPayment({
-//         elements,
-//         confirmParams: {
-//           // Make sure to change this to your payment completion page
-//           return_url: "http://localhost:3000/confirmation",
-//         },
-//       });
-//     } catch (error) {
-//       console.log(error)
-//       if (error.type === "card_error" || error.type === "validation_error") {
-//         setMessage(error.message);
-//       } else {
-//         setMessage("An unexpected error occurred.");
-//       }
-//     }
-
-//     // This point will only be reached if there is an immediate error when
-//     // confirming the payment. Otherwise, your customer will be redirected to
-//     // your `return_url`. For some payment methods like iDEAL, your customer will
-//     // be redirected to an intermediate site first to authorize the payment, then
-//     // redirected to the `return_url`.
-
-//     setIsLoading(false);
-//   };
-
-//   return (
-//     <form id="payment-form" onSubmit={handleSubmit}>
-//       <PaymentElement id="payment-element" />
-//       <button disabled={isLoading || !stripe || !elements} id="submit">
-//         <span id="button-text">
-//           {isLoading ? <div className="spinner" id="spinner"></div> : "Pay now"}
-//         </span>
-//       </button>
-//       {/* Show any error or success messages */}
-//       {message && <div id="payment-message">{message}</div>}
-//     </form>
-//   );
-// };
--- src/components/stripe/StripeCheckoutElement.js
+++ src/components/stripe/StripeCheckoutElement.js
@@ -2,31 +2,59 @@ import React, { useState, useEffect } from "react";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 import { StripeCheckoutForm } from "./StripeCheckForm";
-// import './checkoutStyles.css'
-
-
+import { useDeviceCount } from "../../hooks/useDeviceCountStore";
+import { devitrackApi, devitrackApiStripe } from "../../apis/devitrackApi";
+const stripePromise = loadStripe(
+  "pk_test_51LkbrKA4UM3TTNMjsa6TvhL78emkZK3YyHWYsE9n95zfrDNFNPcCQAwZz4ZK4XHyODZimzeU7I1PEfK9OcYJLb8N0010TL3oW6"
+);
 // Make sure to call loadStripe outside of a component’s render to avoid
 // recreating the Stripe object on every render.
 // This is your test publishable API key.
-const stripePromise = loadStripe("pk_test_51LkbrKA4UM3TTNMjsa6TvhL78emkZK3YyHWYsE9n95zfrDNFNPcCQAwZz4ZK4XHyODZimzeU7I1PEfK9OcYJLb8N0010TL3oW6");
 
 export const StripeCheckoutElement = () => {
-  
+  const { device } = useDeviceCount();
+  const token = localStorage.getItem("token");
   const [clientSecret, setClientSecret] = useState("");
+  const [paymentIntentId, setPaymentIntentId] = useState("");
+  const [data, setData] = useState(null);
+  const [first, setFirst] = useState(false);
 
   useEffect(() => {
-    // Create PaymentIntent as soon as the page loads
-    fetch("http://localhost:34001/api/stripe/create-payment-intent", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ amount: 20000 }), /**items: [{ id: "xl-tshirt" }] */
-    })
-      .then((res) => res.json())
-      .then((data) => setClientSecret(data.clientSecret));
+    // // Create PaymentIntent as soon as the page loads
+    if (
+      // first
+      !token ||
+      token.length > 2
+    ) {
+      const { data } = devitrackApi
+        .post("/stripe/create-payment-intent", {
+          device: device,
+        })
+        .then((data) => {
+          console.log("data effect", { data });
+          setData(data);
+          setClientSecret(data.data.clientSecret);
+          setPaymentIntentId(data.data.payment_intent_id);
+        });
+    }
   }, []);
+  console.log({ token });
+  console.log("data log", data);
+
+  useEffect(() => {
+    if (token.length > 2) {
+      const { data } = devitrackApiStripe.post(
+        `/payment-intent/${paymentIntentId}`,
+        {
+          device,
+          paymentIntentId,
+        }
+      );
+    }
+  }, [device]);
 
   const appearance = {
-    theme: 'stripe',
+    theme: "flat",
   };
   const options = {
     clientSecret,
@@ -42,16 +70,4 @@ export const StripeCheckoutElement = () => {
       )}
     </div>
   );
-}
-
-
-
-
-
-
-
-
-
-
-
-
+};
