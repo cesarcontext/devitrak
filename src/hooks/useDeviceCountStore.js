d5b434b468457b454bc7bbad4e18ddc5f9e84e67
grodriguezcontextglobal <grodriguez@contextglobal.com>
2022-09-28 17:15:52 -0400

    device state saved in localStorage to keep it after be refreshed

--- src/hooks/useDeviceCountStore.js
+++ src/hooks/useDeviceCountStore.js
@@ -4,39 +4,35 @@ import { increment, decrement, reset } from "../store/slices/deviceSlice";
 
 export const useDeviceCount = () => {
   const device = useSelector((state) => state.device.value);
+  const deviceSelected = localStorage.getItem("device")
   const dispatch = useDispatch();
-
-  localStorage.setItem("device", device);
 
   const handleIncreaseDevice = (event) => {
     event.preventDefault();
     dispatch(increment());
+    localStorage.setItem("device", deviceSelected);
   };
 
   const handleDecreaseDevice = (event) => {
     event.preventDefault();
     dispatch(decrement());
+    localStorage.setItem("device", deviceSelected);
 
-    if (device <= 0) {
-      return dispatch(reset());
+    if (device <= 1) {
+      dispatch(reset());
+      localStorage.setItem("device", deviceSelected);
     }
   };
 
   const handleResetDevice = (event) => {
     event.preventDefault();
     dispatch(reset());
+    localStorage.setItem("device", deviceSelected);
   };
-
-  const amountToDeposit = device * 200;
-
-  const deviceSelected = localStorage.getItem("device");
-  const deviceRented = Array.from(Array(parseInt(deviceSelected)), (_, x) => x);
 
   return {
     //* Propiedades
     device,
-    amountToDeposit,
-    deviceRented,
     deviceSelected,
 
     //* Métodos
