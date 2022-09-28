49c3d6b87efa9f4850c91f3a4808d22bb91c2345
grodriguezcontextglobal <grodriguez@contextglobal.com>
2022-09-28 17:15:07 -0400

    reducer to render all payment intents created

--- src/hooks/useStripeHook.js
+++ src/hooks/useStripeHook.js
@@ -1,9 +1,44 @@
-import { devitrackApiStripe } from "../apis/devitrackApi";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { devitrackApiStripe, devitrackApi } from "../apis/devitrackApi";
+import { onAddNewPaymentIntent } from "../store/slices/stripeSlice";
+import { useDeviceCount } from "./useDeviceCountStore";
 
 export const useStripeHook = () => {
-  const saveStripeTransaction = ({ payment_intent, clientSecret, device }) => {
+  const { paymentIntent } = useSelector((state) => state.stripe);
+  const dispatch = useDispatch();
+  const { device } = useDeviceCount();
+  const [data, setData] = useState(null);
+  const [clientSecret, setClientSecret] = useState(null);
+  const [visibleButton, setVisibleButton] = useState("content")
+
+  const startStripePaymentIntent = async (request, response) => {
     try {
-      const response = devitrackApiStripe
+      const response = await devitrackApi
+        .post("/stripe/create-payment-intent", {
+          device: device,
+        })
+        .then((data) => {
+          console.log("data effect", { data });
+          setData(data);
+          setClientSecret(data.data.clientSecret);
+          setVisibleButton("none")
+          if (data) {
+            dispatch(onAddNewPaymentIntent(data));
+          }
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const saveStripeTransaction = async ({
+    payment_intent,
+    clientSecret,
+    device,
+  }) => {
+    try {
+      const response = await devitrackApiStripe
         .post("/stripe-transaction", {
           paymentIntent: payment_intent,
           clientSecret,
@@ -18,10 +53,26 @@ export const useStripeHook = () => {
     }
   };
 
+  const listAllPaymentIntent = async () => {
+    try {
+      const { data } = await devitrackApi.get("/stripe/payment_intents")
+
+      console.log('data received', data )
+    } catch (error) {
+      console.log( error )
+    }
+  };
+  console.log("payment intent state", paymentIntent);
+
   return {
     //* Propiedades
+    paymentIntent,
+    clientSecret,
+    visibleButton,
 
     //* Métodos
+    startStripePaymentIntent,
     saveStripeTransaction,
+    listAllPaymentIntent,
   };
 };
