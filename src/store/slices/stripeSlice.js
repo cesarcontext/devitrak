daa19e10f4deea4d711aa1fa95ad7e1dabe63649
grodriguezcontextglobal <grodriguez@contextglobal.com>
2022-09-28 17:18:52 -0400

    stripe slice created to manege info from the top of the app

---
+++ src/store/slices/stripeSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const stripeSlice = createSlice({
+  name: "stripe",
+  initialState: {
+    paymentIntent: []
+  },
+  reducers: {
+    onAddNewPaymentIntent: (state, { payload }) => {
+        state.paymentIntent.push(payload.data)
+    }
+  },
+});
+
+export default stripeSlice.reducer;
+
+// Action creators are generated for each case reducer function
+export const { onAddNewPaymentIntent } = stripeSlice.actions;
