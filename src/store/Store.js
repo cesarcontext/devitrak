a93f734a17a3fd5ef629a85ae2b40edef6cbef97
grodriguezcontextglobal <grodriguez@contextglobal.com>
2022-09-30 18:11:18 -0400

    redux persist functions added to store to keep data after page is refreshed and be able to dispatch data to everywhere in app

--- src/store/Store.js
+++ src/store/Store.js
@@ -1,18 +1,29 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import adminSlice from "./slices/adminSlice";
 import contactInfoSlice from "./slices/contactInfoSlice";
 import deviceSlice from "./slices/deviceSlice";
 import paymentInfoSlice from "./slices/paymentInfoSlice";
 import stripeSlice from "./slices/stripeSlice";
 import uiSlice from "./slices/uiSlice";
+import storage from "redux-persist/es/storage"
+import {persistReducer} from "redux-persist"
 
-export const store = configureStore({
-  reducer: {
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage
+}
+
+const reducers = combineReducers({
     device: deviceSlice,
     ui: uiSlice,
     contactInfo: contactInfoSlice,
     paymentInfo: paymentInfoSlice,
     admin: adminSlice,
     stripe: stripeSlice,
-  },
+})
+
+const persistedReducers = persistReducer(persistConfig, reducers)
+export const store = configureStore({
+  reducer: persistedReducers,
 });
