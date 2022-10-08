37b1fc815df634c2d55d455063737686b402a93e
grodriguezcontextglobal <grodriguez@contextglobal.com>
2022-10-08 17:35:27 -0400

    localstorage setitem removed from all reducers

--- src/hooks/useDeviceCountStore.js
+++ src/hooks/useDeviceCountStore.js
@@ -4,36 +4,30 @@ import { increment, decrement, reset } from "../store/slices/deviceSlice";
 
 export const useDeviceCount = () => {
   const device = useSelector((state) => state.device.value);
-  const deviceSelected = localStorage.getItem("device")
   const dispatch = useDispatch();
 
   const handleIncreaseDevice = (event) => {
     event.preventDefault();
     dispatch(increment());
-    localStorage.setItem("device", deviceSelected);
   };
 
   const handleDecreaseDevice = (event) => {
     event.preventDefault();
     dispatch(decrement());
-    localStorage.setItem("device", deviceSelected);
 
     if (device <= 1) {
       dispatch(reset());
-      localStorage.setItem("device", deviceSelected);
     }
   };
 
   const handleResetDevice = (event) => {
     event.preventDefault();
     dispatch(reset());
-    localStorage.setItem("device", deviceSelected);
   };
 
   return {
     //* Propiedades
     device,
-    deviceSelected,
 
     //* Métodos
     handleIncreaseDevice,
