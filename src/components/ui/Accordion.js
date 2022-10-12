65780d6caf08007ed7171733ff554716e929b730
grodriguezcontextglobal <grodriguez@contextglobal.com>
2022-10-12 08:39:26 -0400

    function to check payment intent id/user have receivers assigned and render them

--- src/components/ui/Accordion.js
+++ src/components/ui/Accordion.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useAdminStore } from "../../hooks/useAdminStore";
 import { useDeviceCount } from "../../hooks/useDeviceCountStore";
+import { useStripeHook } from "../../hooks/useStripeHook";
 
 export const Accordion = () => {
-  const { device } = useDeviceCount();
+  const { paymentIntent } = useStripeHook();
+  const { paymentIntentReceiversAssigned } = useSelector(
+    (state) => state.stripe
+  );
+  const { checkReceiversAssignedToPaymentIntent } = useAdminStore();
+
+  const paymentToCheck = paymentIntent.at(-1).data.payment_intent_id;
+
+  console.log("paymentToCheck", paymentToCheck);
+
+  useEffect(() => {
+    checkReceiversAssignedToPaymentIntent(paymentToCheck);
+  }, []);
   return (
     <div>
       <div style={{ width: "50%", margin: "auto", border: "solid 1px #fff" }}>
@@ -27,14 +42,37 @@ export const Accordion = () => {
               data-bs-parent="#accordionExample"
             >
               <div className="accordion-body">
-                {new Array( device).fill(<div style={{display: "flex", flexDirection: "column"}}><input className="input-field" disabled /></div>)}
+                {paymentIntentReceiversAssigned.length < 1 ? (
+                  <h6>No receivers assigned yet</h6>
+                ) : (
+                  paymentIntentReceiversAssigned
+                    .at(-1)
+                    .device.map((index, receiver) => {
+                      return (
+                        <tbody style={{width: "100%", display: "flex", justifyContent: "center", alignItems: "center"}}>
+                          <tr style={{width: "100%", display: "flex", justifyContent: "space-between", alignItems: "center"}}>
+                            <th scope="row">{receiver + 1}</th>
+                            <td>{index}</td>
+                          </tr>
+                        </tbody>
+                      );
+                    })
+                )}
+                {/* {new Array( device).fill(<div style={{display: "flex", flexDirection: "column"}}><input className="input-field" disabled /></div>)} */}
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div style={{ width: "50%",marginTop: "3%", margin: "auto", border: "solid 1px #fff" }}>
-        <div className="accordion" id="accordionExample">
+      <div
+        style={{
+          width: "50%",
+          marginTop: "3%",
+          margin: "auto",
+          border: "solid 1px #fff",
+        }}
+      >
+        {/* <div className="accordion" id="accordionExample">
           <div className="accordion-item">
             <h2 className="accordion-header" id="headingTwo">
               <button
@@ -57,7 +95,7 @@ export const Accordion = () => {
               <div className="accordion-body"></div>
             </div>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
