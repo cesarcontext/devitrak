c47f82237caa21fe4347e66e40198bb5131591bc
grodriguezcontextglobal <grodriguez@contextglobal.com>
2022-11-04 18:15:35 -0400

    contract contained to be exported

---
+++ src/components/ui/PrivacyPolicyContract.js
@@ -0,0 +1,111 @@
+import React from "react";
+
+export const PrivacyPolicyContract = () => {
+  return (
+    <div
+      style={{
+        textAlign: "justify",
+      }}
+    >
+      <span>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Diam quis enim
+        lobortis scelerisque fermentum dui faucibus in ornare. Aliquam faucibus
+        purus in massa. Enim facilisis gravida neque convallis a cras. Augue
+        eget arcu dictum varius duis at consectetur lorem. Viverra mauris in
+        aliquam sem. Luctus venenatis lectus magna fringilla urna porttitor
+        rhoncus. Id cursus metus aliquam eleifend mi in. Dignissim convallis
+        aenean et tortor at. Nunc aliquet bibendum enim facilisis gravida neque
+        convallis a cras. Ullamcorper malesuada proin libero nunc. Id eu nisl
+        nunc mi ipsum faucibus vitae aliquet. Semper feugiat nibh sed pulvinar
+        proin gravida hendrerit. Faucibus purus in massa tempor nec feugiat nisl
+        pretium. Amet consectetur adipiscing elit pellentesque habitant morbi
+        tristique senectus et. Erat nam at lectus urna duis convallis. Duis at
+        consectetur lorem donec massa sapien faucibus et molestie. Tellus in
+        metus vulputate eu scelerisque felis imperdiet proin fermentum. Non diam
+        phasellus vestibulum lorem. Aliquet nibh praesent tristique magna sit
+        amet. Tempor orci eu lobortis elementum nibh tellus molestie nunc.
+        Nascetur ridiculus mus mauris vitae. Aliquam vestibulum morbi blandit
+        cursus risus at ultrices mi tempus. Eleifend donec pretium vulputate
+        sapien nec sagittis aliquam. Tellus orci ac auctor augue mauris augue
+        neque. Morbi non arcu risus quis varius quam quisque id. Faucibus
+        pulvinar elementum integer enim neque volutpat ac. In iaculis nunc sed
+        augue lacus viverra vitae congue. In nibh mauris cursus mattis. Auctor
+        augue mauris augue neque gravida in fermentum. Libero enim sed faucibus
+        turpis. Etiam dignissim diam quis enim lobortis scelerisque fermentum.
+        Senectus et netus et malesuada fames. Non odio euismod lacinia at quis
+        risus. Porttitor lacus luctus accumsan tortor. Odio facilisis mauris sit
+        amet massa vitae tortor condimentum lacinia. Urna porttitor rhoncus
+        dolor purus non enim praesent elementum facilisis. Arcu odio ut sem
+        nulla pharetra diam sit. Leo duis ut diam quam nulla porttitor massa id
+        neque. A condimentum vitae sapien pellentesque habitant morbi tristique
+        senectus et. Augue interdum velit euismod in pellentesque massa placerat
+        duis ultricies. Aenean euismod elementum nisi quis eleifend. Aliquam id
+        diam maecenas ultricies. Sollicitudin tempor id eu nisl nunc mi ipsum.
+        Cras ornare arcu dui vivamus arcu felis bibendum ut tristique. A diam
+        sollicitudin tempor id eu nisl nunc mi. Tincidunt praesent semper
+        feugiat nibh sed pulvinar. Aliquam purus sit amet luctus venenatis
+        lectus. Pretium lectus quam id leo in vitae turpis massa. Suspendisse
+        ultrices gravida dictum fusce ut. Netus et malesuada fames ac turpis
+        egestas integer eget. Libero id faucibus nisl tincidunt. Sagittis eu
+        volutpat odio facilisis mauris. Leo vel fringilla est ullamcorper eget
+        nulla facilisi etiam dignissim. In tellus integer feugiat scelerisque
+        varius morbi enim nunc faucibus. In fermentum et sollicitudin ac orci
+        phasellus egestas tellus. Augue mauris augue neque gravida in. Dictumst
+        vestibulum rhoncus est pellentesque. Placerat orci nulla pellentesque
+        dignissim. Aliquet lectus proin nibh nisl condimentum. Cras adipiscing
+        enim eu turpis. Rhoncus mattis rhoncus urna neque viverra justo nec.
+        Ligula ullamcorper malesuada proin libero nunc consequat interdum
+        varius. Blandit volutpat maecenas volutpat blandit aliquam etiam. Tellus
+        elementum sagittis vitae et leo. A pellentesque sit amet porttitor eget
+        dolor. Augue lacus viverra vitae congue eu consequat ac felis donec.
+        Fermentum posuere urna nec tincidunt praesent semper feugiat nibh.
+        Tincidunt nunc pulvinar sapien et ligula. Erat pellentesque adipiscing
+        commodo elit at imperdiet dui accumsan sit. Sodales neque sodales ut
+        etiam sit. Donec pretium vulputate sapien nec. Mattis aliquam faucibus
+        purus in massa tempor nec feugiat. Sed euismod nisi porta lorem mollis
+        aliquam ut porttitor leo. Sed euismod nisi porta lorem mollis aliquam.
+        Amet facilisis magna etiam tempor orci eu. Tellus id interdum velit
+        laoreet id donec ultrices tincidunt. Sed risus pretium quam vulputate
+        dignissim suspendisse in. Dui faucibus in ornare quam viverra orci. Enim
+        nulla aliquet porttitor lacus. Viverra nibh cras pulvinar mattis nunc
+        sed. At imperdiet dui accumsan sit. Felis eget velit aliquet sagittis id
+        consectetur. Pulvinar sapien et ligula ullamcorper malesuada proin
+        libero nunc. Velit dignissim sodales ut eu sem integer. Semper feugiat
+        nibh sed pulvinar proin gravida hendrerit lectus a. Neque convallis a
+        cras semper auctor. In ornare quam viverra orci. Bibendum ut tristique
+        et egestas quis ipsum suspendisse ultrices. Sed lectus vestibulum mattis
+        ullamcorper velit sed ullamcorper. Ac turpis egestas integer eget
+        aliquet nibh. Eget magna fermentum iaculis eu non diam phasellus
+        vestibulum lorem. Lectus magna fringilla urna porttitor. Odio aenean sed
+        adipiscing diam. Euismod in pellentesque massa placerat duis ultricies
+        lacus sed turpis. Vitae aliquet nec ullamcorper sit amet risus. Ipsum
+        faucibus vitae aliquet nec ullamcorper sit amet risus. Quis commodo odio
+        aenean sed adipiscing. Tincidunt dui ut ornare lectus sit amet est. Urna
+        molestie at elementum eu facilisis sed. Morbi tempus iaculis urna id.
+        Accumsan lacus vel facilisis volutpat est velit egestas dui. Ullamcorper
+        a lacus vestibulum sed arcu non odio euismod lacinia. Proin fermentum
+        leo vel orci. Ipsum dolor sit amet consectetur adipiscing elit
+        pellentesque habitant. Viverra vitae congue eu consequat ac felis donec
+        et odio. Vel pharetra vel turpis nunc. Tincidunt arcu non sodales neque
+        sodales ut. Nunc mattis enim ut tellus elementum sagittis vitae. Rhoncus
+        urna neque viverra justo nec ultrices dui sapien. Aenean euismod
+        elementum nisi quis. Adipiscing elit pellentesque habitant morbi
+        tristique senectus. Malesuada pellentesque elit eget gravida cum sociis
+        natoque penatibus. Viverra tellus in hac habitasse platea dictumst
+        vestibulum. Quisque egestas diam in arcu. Velit ut tortor pretium
+        viverra suspendisse potenti. Nunc mattis enim ut tellus elementum
+        sagittis vitae et leo. Eu consequat ac felis donec et odio pellentesque
+        diam. Vitae sapien pellentesque habitant morbi tristique senectus et
+        netus. Egestas maecenas pharetra convallis posuere morbi leo. Mauris
+        commodo quis imperdiet massa tincidunt nunc. Eget nulla facilisi etiam
+        dignissim diam quis. Eget nunc lobortis mattis aliquam faucibus purus.
+        Ornare arcu odio ut sem. Varius morbi enim nunc faucibus a pellentesque.
+        Vel fringilla est ullamcorper eget nulla. A diam sollicitudin tempor id
+        eu nisl nunc mi. Elit pellentesque habitant morbi tristique. Morbi
+        tristique senectus et netus et malesuada fames. Fermentum leo vel orci
+        porta. Sit amet massa vitae tortor condimentum.{" "}
+      </span>
+    </div>
+  );
+};
