1c5b5fafd153f399758e44d88622e36b39fbad4a
grodriguezcontextglobal <grodriguez@contextglobal.com>
2022-11-04 18:19:31 -0400

    modal component imported

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Navbar } from "../components/ui/Navbar";
 import { ContactInfo } from "../components/contact/ContactInfo";
+import { UserConsentPrivacyPolicyModal } from "../components/ui/UserConsentPrivacyPolicyModal";
 
 
 export const Home = () => {
@@ -8,6 +9,7 @@ export const Home = () => {
     <>
       <Navbar /> 
       <ContactInfo />
+      <UserConsentPrivacyPolicyModal />
     </>
   );
 };
