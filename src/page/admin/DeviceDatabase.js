63ac9e18958098ee4e61a5b2a6a155f2a759cdfa
grodriguezcontextglobal <grodriguez@contextglobal.com>
2022-11-04 18:18:09 -0400

    receiverStock component imported

--- src/page/admin/DeviceDatabase.js
+++ src/page/admin/DeviceDatabase.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { ReceiverStock } from "../../components/admin/DeviceDatabaseSection/ReceiverStock";
 import { Navbar } from "../../components/admin/ui/Navbar";
 
 export const DeviceDatabase = () => {
@@ -6,6 +7,9 @@ export const DeviceDatabase = () => {
     <div>
       <Navbar />
       <h2>Database</h2>
+      <div>
+        <ReceiverStock />
+      </div>
     </div>
   );
 };
