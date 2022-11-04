530992baf388180cd0575ec350a0b7253ec56118
grodriguezcontextglobal <grodriguez@contextglobal.com>
2022-11-04 18:21:07 -0400

    slice to contain selection of privacy policy and passing to entire tree

---
+++ src/store/slices/privacyPolicyUserResponseSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const response = false
+
+const privacyPolicyUserResponseSlice = createSlice({
+  name: "privacyPolicyUserResponse",
+  initialState: {
+    response
+  },
+  reducers: {
+    onUserPrivacyPolicyResponse: (state, { payload }) => {
+      state.response = payload;
+    },
+  },
+});
+
+export default privacyPolicyUserResponseSlice.reducer;
+
+// Action creators are generated for each case reducer function
+export const { onUserPrivacyPolicyResponse } =
+  privacyPolicyUserResponseSlice.actions;
