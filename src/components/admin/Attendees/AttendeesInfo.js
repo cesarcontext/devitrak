fd6dc3c2db0d0244ca348f1007dcedb69b3b163e
grodriguezcontextglobal <grodriguez@contextglobal.com>
2022-11-08 17:37:00 -0500

    display and send data from No-regular User

--- src/components/admin/Attendees/AttendeesInfo.js
+++ src/components/admin/Attendees/AttendeesInfo.js
@@ -1,20 +1,26 @@
 import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { NavLink } from "react-router-dom";
 import { devitrackApi } from "../../../apis/devitrackApi";
+import { ModalCreateUser } from "../ui/ModalCreateUser";
 import { Pagination } from "../ui/Pagination";
 import { StripeTransactionHistoryByUser } from "./StripeTransactionHistoryByUser";
+import { onAddPaymentIntentDetailSelected } from "../../../store/slices/stripeSlice";
 
 export const AttendeesInfo = ({ searchTerm }) => {
   const [users, setUsers] = useState([]);
   const [sendObjectIdUser, setSendObjectIdUser] = useState();
+  const [createUserButton, setCreateUserButton] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const [usersRenderedPerPage] = useState(4);
+  const dispatch = useDispatch()
 
   useEffect(() => {
     devitrackApi
       .get("/auth/users")
       .then((response) => response.data)
       .then((data) => setUsers(data.users));
-  }, []);
+  }, [createUserButton]);
 
   const indexOfLastUsersRendered = currentPage * usersRenderedPerPage;
   const indexOfFirstUsersRendered =
@@ -97,12 +103,26 @@ export const AttendeesInfo = ({ searchTerm }) => {
                     );
                   })}
           </table>
-          <div>
-            <Pagination
-              childrenRenderedPerPage={usersRenderedPerPage}
-              totalChildren={users.length}
-              paginate={paginate}
-            />
+          <div
+            style={{
+              display: "flex",
+              flexWrap: "wrap",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <div>
+              <Pagination
+                childrenRenderedPerPage={usersRenderedPerPage}
+                totalChildren={users.length}
+                paginate={paginate}
+              />
+            </div>
+            <div>
+              <button onClick={() => setCreateUserButton(true)}>
+                Create user
+              </button>
+            </div>
           </div>
         </div>
       </div>
@@ -121,14 +141,16 @@ export const AttendeesInfo = ({ searchTerm }) => {
         <div>
           <h2>Details</h2>
         </div>
-        <div style={{
-              width: "100%",
-              padding: "25px",
-              display: "flex",
-              justifyContent: "space-between",
-              alignItems: "center",
-              textAlign: "left",
-            }}>
+        <div
+          style={{
+            width: "100%",
+            padding: "25px",
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            textAlign: "left",
+          }}
+        >
           {" "}
           <div
             style={{
@@ -160,17 +182,62 @@ export const AttendeesInfo = ({ searchTerm }) => {
                 );
               }
             })}
-          </div>        
+          </div>
         </div>
         <div style={{ width: "100%" }}>
           <div>Transactions</div>
-          <div style={{ width: "100%" }}>
-            <StripeTransactionHistoryByUser
-              sendObjectIdUser={sendObjectIdUser}
-            />
-          </div>
+          {users?.map((user) => {
+            if (user.id === sendObjectIdUser) {
+              if (user.category === "Regular") {
+                return (
+                  <div key={user.id}>
+                    <StripeTransactionHistoryByUser
+                      sendObjectIdUser={sendObjectIdUser}
+                    />
+                  </div>
+                );
+              }
+              if (user.category === "No-regular") {
+                return (
+                  <div key={user.id}>
+                    <div
+                      style={{
+                        width: "50%",
+                        display: "flex",
+                        flexWrap: "wrap",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                        margin: "0 auto"
+                      }}
+                    >
+                      <div>
+                        <h6>User Category</h6>
+                        {user.category}
+                      </div>
+                      <div>
+                        <h6>Device</h6>
+                        <NavLink to="/admin/attendees/receiver_assignation">
+                          <button style={{ width: "90%", padding: "5px" }}
+                          onClick={() => 
+                            dispatch(
+                            onAddPaymentIntentDetailSelected({user})
+                          )}>
+                            Assign Device
+                          </button>
+                        </NavLink>
+                      </div>
+                    </div>
+                  </div>
+                );
+              }
+            }
+          })}
         </div>
-    </div>
+      </div>
+      <ModalCreateUser
+        createUserButton={createUserButton}
+        setCreateUserButton={setCreateUserButton}
+      />
     </div>
   );
 };
