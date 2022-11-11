a408c30acddd44e81f4cb83bc720f4be403f4e58
grodriguezcontextglobal <grodriguez@contextglobal.com>
2022-11-11 11:49:30 -0500

    buttons added into div

--- src/page/MyProfile.js
+++ src/page/MyProfile.js
@@ -1,6 +1,5 @@
 import React, { useState, useMemo, useEffect } from "react";
 import Swal from "sweetalert2";
-import { Accordion } from "../components/ui/Accordion";
 import { ContactInfoProfile } from "../components/contact/ContactInfoProfile";
 import { NavbarBottom } from "../components/ui/NavbarBottom";
 import { ReturnDeviceAlert } from "../components/ui/ReturnDeviceAlert";
@@ -161,81 +160,87 @@ export const MyProfile = () => {
           <h3>Account</h3>
         </div>
         <div className="container-box-user-info-detail">
-          <div className="box-user-detail-buttons">
-            {showInfo !== true ? (
-              <button
-                disabled={buttonState}
-                id="box-user-detail-edit-button"
-                onClick={handleButtonState}
-              >
-                <h5>
-                  EDIT{" "}
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="15"
-                    height="15"
-                    fill="currentColor"
-                    className="bi bi-pencil"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
-                  </svg>
-                </h5>
-              </button>
-            ) : (
-              <div
-                style={{
-                  width: "100%",
-                  display: "flex",
-                  justifyContent: "space-between",
-                }}
-              >
+          <div style={{ width: "100%", display: "flex" }}>
+            <div className="box-user-detail-buttons">
+              {showInfo !== true ? (
                 <button
-                  className="col"
-                  id="box-user-detail-cancel-button"
+                  disabled={buttonState}
+                  id="box-user-detail-edit-button"
                   onClick={handleButtonState}
                 >
                   <h5>
-                    CANCEL{" "}
-                    <svg
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        clipRule="evenodd"
-                        d="M17 5V4C17 2.89543 16.1046 2 15 2H9C7.89543 2 7 2.89543 7 4V5H4C3.44772 5 3 5.44772 3 6C3 6.55228 3.44772 7 4 7H5V18C5 19.6569 6.34315 21 8 21H16C17.6569 21 19 19.6569 19 18V7H20C20.5523 7 21 6.55228 21 6C21 5.44772 20.5523 5 20 5H17ZM15 4H9V5H15V4ZM17 7H7V18C7 18.5523 7.44772 19 8 19H16C16.5523 19 17 18.5523 17 18V7Z"
-                        fill="currentColor"
-                      />
-                      <path d="M9 9H11V17H9V9Z" fill="currentColor" />
-                      <path d="M13 9H15V17H13V9Z" fill="currentColor" />
-                    </svg>
-                  </h5>
-                </button>
-                <button
-                  className="col"
-                  id="box-user-detail-save-button"
-                  onClick={handleEditContactInfo}
-                >
-                  <h5>
-                    SAVE{" "}
+                    EDIT{" "}
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       width="15"
                       height="15"
                       fill="currentColor"
-                      className="bi bi-save"
+                      className="bi bi-pencil"
                       viewBox="0 0 16 16"
                     >
-                      <path d="M2 1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H9.5a1 1 0 0 0-1 1v7.293l2.646-2.647a.5.5 0 0 1 .708.708l-3.5 3.5a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L7.5 9.293V2a2 2 0 0 1 2-2H14a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h2.5a.5.5 0 0 1 0 1H2z" />
+                      <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
                     </svg>
                   </h5>
                 </button>
-              </div>
-            )}
+              ) : (
+                <div
+                  style={{
+                    width: "100%",
+                    display: "flex",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <div className="col">
+                    <button
+                      className="col"
+                      id="box-user-detail-cancel-button"
+                      onClick={handleButtonState}
+                    >
+                      <h5>
+                        CANCEL{" "}
+                        <svg
+                          width="24"
+                          height="24"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            clipRule="evenodd"
+                            d="M17 5V4C17 2.89543 16.1046 2 15 2H9C7.89543 2 7 2.89543 7 4V5H4C3.44772 5 3 5.44772 3 6C3 6.55228 3.44772 7 4 7H5V18C5 19.6569 6.34315 21 8 21H16C17.6569 21 19 19.6569 19 18V7H20C20.5523 7 21 6.55228 21 6C21 5.44772 20.5523 5 20 5H17ZM15 4H9V5H15V4ZM17 7H7V18C7 18.5523 7.44772 19 8 19H16C16.5523 19 17 18.5523 17 18V7Z"
+                            fill="currentColor"
+                          />
+                          <path d="M9 9H11V17H9V9Z" fill="currentColor" />
+                          <path d="M13 9H15V17H13V9Z" fill="currentColor" />
+                        </svg>
+                      </h5>
+                    </button>
+                  </div>
+                  <div className="col">
+                    <button
+                      className="col"
+                      id="box-user-detail-save-button"
+                      onClick={handleEditContactInfo}
+                    >
+                      <h5>
+                        SAVE{" "}
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="15"
+                          height="15"
+                          fill="currentColor"
+                          className="bi bi-save"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M2 1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H9.5a1 1 0 0 0-1 1v7.293l2.646-2.647a.5.5 0 0 1 .708.708l-3.5 3.5a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L7.5 9.293V2a2 2 0 0 1 2-2H14a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h2.5a.5.5 0 0 1 0 1H2z" />
+                        </svg>
+                      </h5>
+                    </button>
+                  </div>
+                </div>
+              )}
+            </div>
           </div>
           <div>
             <h5>Your conntact infomartion</h5>
@@ -309,9 +314,7 @@ export const MyProfile = () => {
         <div className="container-device-alert">
           <ReturnDeviceAlert />
         </div>
-        <div
-         className="container-device-accordion-List"
-        >
+        <div className="container-device-accordion-List">
           <div>
             <h3>Your devices</h3>
             <span>All the devices with checkmarks are now returned</span>
