64a67446b9071c87dfe9d1f4317e1ad0980efdf8
grodriguezcontextglobal <grodriguez@contextglobal.com>
2022-11-17 09:17:30 -0500

    props destructured in map function and Math floor function added to generate an unique number as key

--- src/helper/UserTable.js
+++ src/helper/UserTable.js
@@ -1,34 +1,31 @@
-import React from 'react';
+import React from "react";
 
 export const UserTable = ({ headers, listOfReceiver }) => {
-    return (
-        <div>
-            <table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
-                <thead>
-                    <tr>
-                        {headers.map(row => {
-                            return (
-                                <th style={{ "fontWeight": "bold" }}>{row.label}</th>
-                            )
-                        })}
-                    </tr>
-                </thead>
-                <tbody>
-                    {listOfReceiver.map((row, index) => (
-                        <tr
-                            key={row.id+index}
-                        >
-                            <td>{(row.index) + 1}</td>
-                            <td>{row.device}</td>
-                            <td>{row.status}</td>
-                            <td>{row.activity}</td>
-                            <td>{row.comment}</td>
-                            <td>{row.user}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
-    );
-}
 
+    const max = 2^53
+  return (
+    <div>
+      <table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+        <thead>
+          <tr>
+            {headers.map((row) => {
+              return <th style={{ fontWeight: "bold" }}>{row.label}</th>;
+            })}
+          </tr>
+        </thead>
+        {listOfReceiver.map(({id, device, status, activity, comment, user, index}) => (
+          <tbody key={index}>
+            <tr key={id * Math.floor(Math.random() * max)}>
+              <td>{index + 1}</td>
+              <td>{device}</td>
+              <td>{status}</td>
+              <td>{activity}</td>
+              <td>{comment}</td>
+              <td>{user}</td>
+            </tr>
+          </tbody>
+        ))}
+      </table>
+    </div>
+  );
+};
