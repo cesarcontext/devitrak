3687d38a30fd2d176d7fddc4547cdb443dd944ca
grodriguezcontextglobal <grodriguez@contextglobal.com>
2022-11-17 09:15:09 -0500

    map function to render the user removed, user info imported from store

--- src/components/contact/ContactInfoProfile.js
+++ src/components/contact/ContactInfoProfile.js
@@ -3,61 +3,55 @@ import { useContactInfoStore } from "../../hooks/useContactInfoStore";
 import "../../style/component/contact/ContactInfoProfile.css"
 
 export const ContactInfoProfile = () => {
-  const { users} = useContactInfoStore();
+  const { users } = useContactInfoStore();
   return (
-    <div>
-      {users?.map((item) => {
-        return (
-          <div className="container-user-info-detail-fetched" key={item.email}>
-            <div
-            className="user-info-detail-fullname"
-              
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                className="bi bi-person"
-                viewBox="0 0 16 16"
-              >
-                <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
-              </svg>
-              <div>
-                <span> {item.name}</span>
-                <span> {item.lastName}</span>
-              </div>
-            </div>
-            <div className="user-info-detail-fullname">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                className="bi bi-envelope"
-                viewBox="0 0 16 16"
-              >
-                <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z" />
-              </svg>
-              <span> {item.email}</span>
-            </div>
-            <div className="user-info-detail-fullname">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                className="bi bi-phone"
-                viewBox="0 0 16 16"
-              >
-                <path d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h6zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H5z" />
-                <path d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
-              </svg>
-              <span> {item.phone === undefined ? item.phoneNumber : item.phone}</span>
-            </div>
-          </div>
-        );
-      })}
+    <div className="container-user-info-detail-fetched" key={users.email}>
+    <div
+    className="user-info-detail-fullname"
+      
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="currentColor"
+        className="bi bi-person"
+        viewBox="0 0 16 16"
+      >
+        <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
+      </svg>
+      <div>
+        <span> {users.name}</span>
+        <span> {users.lastName}</span>
+      </div>
     </div>
-  );
+    <div className="user-info-detail-fullname">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="currentColor"
+        className="bi bi-envelope"
+        viewBox="0 0 16 16"
+      >
+        <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z" />
+      </svg>
+      <span> {users.email}</span>
+    </div>
+    <div className="user-info-detail-fullname">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="currentColor"
+        className="bi bi-phone"
+        viewBox="0 0 16 16"
+      >
+        <path d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h6zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H5z" />
+        <path d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
+      </svg>
+      <span> {users.phone === undefined ? users.phoneNumber : users.phone}</span>
+    </div>
+  </div>
+);
 };
