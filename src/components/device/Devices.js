0ae2a9e2c1c7b0fb333f5c26e948ef2c0eefb53d
grodriguezcontextglobal <grodriguez@contextglobal.com>
2022-11-28 11:35:15 -0500

    form fields style set up

--- src/components/device/Devices.js
+++ src/components/device/Devices.js
@@ -3,41 +3,32 @@ import { useDeviceCount } from "../../hooks/useDeviceCountStore";
 import "../../style/component/device/Device.css";
 
 export const Devices = () => {
-  const {
-    device,
-    handleDecreaseDevice,
-    handleIncreaseDevice,
-    // handleResetDevice,
-  } = useDeviceCount();
+  const { device, handleDecreaseDevice, handleIncreaseDevice } =
+    useDeviceCount();
 
   return (
-      <div className="container-device-selection">
-        <div className="row justify-content-center align-items-center">
-          {/* <div className="card shadow-2-strong card-registration"> */}
-          <div className="device-selection">
-            <p>HOW MANY RECEIVERS DO YOU NEED?</p>
-            <div className="button-selection">
-              {/* <div className="p-1"> */}
-                <button id="button-less" onClick={handleDecreaseDevice}>-</button>
-              {/* </div> */}
-              <div className="device-selection-display">
-                <p id="number-device-displayed">{device}</p>
-              </div>
-              {/* <div className="p-1"> */}
-                <button id="button-plus" onClick={handleIncreaseDevice}>+</button>
-              {/* </div> */}
-            </div>
-            {/* </div> */}
+    <div className="col-md-11 m-4 mb-0">
+      <div className="device-selection">
+        <p>HOW MANY RECEIVERS DO YOU NEED?</p>
+        <div className="button-selection">
+          <button id="button-less" onClick={handleDecreaseDevice}>
+            -
+          </button>
+          <div className="device-selection-display">
+            <p id="number-device-displayed">{device}</p>
           </div>
-        </div>
-        <div className="show-amount-section">
-          <p>DEPOSIT TOTAL:</p>
-          <div className="col-2">
-            <h3>
-              <strong>${device * 200}</strong>
-            </h3>
-          </div>
+          <button id="button-plus" onClick={handleIncreaseDevice}>
+            +
+          </button>
         </div>
       </div>
+      <div className="show-amount-section">
+        <div></div>
+        <div className="container-devices-deposit-">
+          <p id="deposit-total">DEPOSIT TOTAL:</p>
+          <p id="amount-displayed">${device * 200}</p>
+        </div>
+      </div>
+    </div>
   );
 };
