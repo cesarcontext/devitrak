38c66c087cd50f465adb3ac202a6f0b0ffb132db
grodriguezcontextglobal <grodriguez@contextglobal.com>
2022-12-12 16:23:32 -0500

    blockLinks reducer dispatched

--- src/page/Authenticate.js
+++ src/page/Authenticate.js
@@ -1,17 +1,19 @@
 import { useStytch, useStytchSession } from "@stytch/stytch-react";
 import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router";
 import { rightDoneMessage } from "../helper/swalFireMessage";
-
+import { blockLinks } from "../store/slices/uiSlice";
 
 export const Authenticate = () => {
   const client = useStytch();
   const session = useStytchSession();
   const navigate = useNavigate();
-
+  const dispatch = useDispatch();
   useEffect(() => {
     if (session) {
       navigate("/checkout");
+      dispatch(blockLinks("auto"))
     } else {
       const token = new URLSearchParams(window.location.search).get("token");
       client.magicLinks
@@ -19,8 +21,9 @@ export const Authenticate = () => {
           session_duration_minutes: 15,
         })
         .then(() => {
-          rightDoneMessage(`Successfully authenticated`)
-          navigate(0) //navigate to 0 to refrese page
+          rightDoneMessage(`Successfully authenticated`);
+          navigate(0); //navigate to 0 to refrese page
+          dispatch(blockLinks("auto"))
         });
     }
   }, [client, session]);
