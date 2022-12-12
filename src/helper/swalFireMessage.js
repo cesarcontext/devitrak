4fdfb8fe9506238fc418c2ab6d0067af5f5ee1cb
grodriguezcontextglobal <grodriguez@contextglobal.com>
2022-12-12 16:02:50 -0500

    component to contain all swal messages for each situation

--- src/helper/swalFireMessage.js
+++ src/helper/swalFireMessage.js
@@ -37,3 +37,23 @@ export const rightDoneMessage = (str) => {
       	`,
   });
 };
+
+
+export const NewUserCreatedMessage = () => {
+  Swal.fire({
+    title: "",
+    width: 600,
+    padding: "3em",
+    text: `Your account was created`,
+    icon: "success",
+    color: "#rgb(30, 115, 190)",
+    background: "#fff",
+    confirmButtonColor: "rgb(30, 115, 190)",
+    backdrop: `
+      	rgb(30, 115, 190)
+        	url("../image/logo.jpg")
+        	left top
+        	no-repeat
+      	`,
+  });
+};
