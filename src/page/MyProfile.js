0efae6d0fa2805859afda0d7e2927cbf39ca8c7a
grodriguezcontextglobal <grodriguez@contextglobal.com>
2022-12-13 12:44:27 -0500

    logout icon removed and labal added

--- src/page/MyProfile.js
+++ src/page/MyProfile.js
@@ -210,7 +210,7 @@ export const MyProfile = () => {
                       onClick={handleButtonState}
                     >
                       <h5>
-                        EDIT{" "}
+                        Edit{" "}
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
                           width="15"
