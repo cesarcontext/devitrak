c4a39c6f0c0e51ef9e17305f7dec7d786212c28e
grodriguezcontextglobal <grodriguez@contextglobal.com>
2022-12-14 09:20:29 -0500

    device message to instruct user for more than 5 devices

--- src/helper/swalFireMessage.js
+++ src/helper/swalFireMessage.js
@@ -57,3 +57,22 @@ export const NewUserCreatedMessage = () => {
       	`,
   });
 };
+
+export const deviceMessageAlert = () => {
+  Swal.fire({
+    title: "",
+    width: 600,
+    padding: "3em",
+    text: `For more than 5 devices, please contact Staff.`,
+    icon: "success",
+    color: "#rgb(30, 115, 190)",
+    background: "#fff",
+    confirmButtonColor: "rgb(30, 115, 190)",
+    backdrop: `
+      	rgb(30, 115, 190)
+        	url("../image/logo.jpg")
+        	left top
+        	no-repeat
+      	`,
+  });
+}
