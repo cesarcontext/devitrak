b7acf95f1b327882caf7bab0772cc3f0a1addfbc
grodriguezcontextglobal <grodriguez@contextglobal.com>
2022-12-22 16:07:06 -0500

    console log removed

--- src/components/passwordless/MagicLink.js
+++ src/components/passwordless/MagicLink.js
@@ -3,12 +3,10 @@ import { rightDoneMessage } from "../../helper/swalFireMessage";
 import "../../style/component/passwrodless/MagicLink.css";
 
 export const MagicLink = (magicLinkParam) => {
-  console.log("🚀 ~ file: MagicLink.js:6 ~ MagicLink ~ magicLinkParam", magicLinkParam)
   
   const client = useStytch();
 
   const emailUserToPassToMagicLink = Object.values(magicLinkParam);
-  console.log("🚀 ~ file: MagicLink.js:11 ~ MagicLink ~ emailUserToPassToMagicLink", emailUserToPassToMagicLink)
   let emailToPass;
   const emailString = emailUserToPassToMagicLink.map(
     (item) => (emailToPass = item)
