65440fe587f70107f29501e4892c2c9ab9590dcf
grodriguezcontextglobal <grodriguez@contextglobal.com>
2022-12-23 18:11:53 -0500

    search field added

--- src/page/admin/DeviceDatabase.js
+++ src/page/admin/DeviceDatabase.js
@@ -1,22 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import { DisplayDataReceiversActivity } from "../../components/admin/DeviceDatabaseSection/DisplayDataReceiversActivity";
 import { DisplayDataReceiversStatus } from "../../components/admin/DeviceDatabaseSection/DisplayDataReceiversStatus";
 import { ReceiverStock } from "../../components/admin/DeviceDatabaseSection/ReceiverStock";
 import { Navbar } from "../../components/admin/ui/Navbar";
-import "../../style/component/admin/DeviceDatabase.css"
+import "../../style/component/admin/DeviceDatabase.css";
+
 export const DeviceDatabase = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
     <div>
       <Navbar />
-      {/* <h2>Database</h2> */}
       <div>
-        <ReceiverStock />
+        <div className="search-div">
+          <div>
+            <h4>Search</h4>
+          </div>
+          <div className="search-input">
+            <input
+              className="search-input-field"
+              name="value"
+              onChange={(e) => setSearchTerm(e.target.value)}
+              value={searchTerm}
+              placeholder="Search"
+            />
+            <i
+              onClick={() => setSearchTerm("")}
+              id="icon-delete-searchTerm"
+              className="bi bi-x"
+            />
+          </div>
+        </div>
+        <ReceiverStock searchTerm={searchTerm} />
       </div>
-      <div className="container-graphic" style={{display:"flex", justifyContent:"space-around", alignItems:"center"}}>
+      <div
+        className="container-graphic"
+        style={{
+          display: "flex",
+          justifyContent: "space-around",
+          alignItems: "center",
+        }}
+      >
         <DisplayDataReceiversActivity />
         <DisplayDataReceiversStatus />
       </div>
     </div>
   );
 };
-        
