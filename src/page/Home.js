4bd98f79c908d4b415022f77edcac9ae68d31e64
grodriguezcontextglobal <grodriguez@contextglobal.com>
2023-01-02 18:43:40 -0500

    AppDownloadModal component removed

--- src/page/Home.js
+++ src/page/Home.js
@@ -3,33 +3,12 @@ import { Navbar } from "../components/ui/Navbar";
 import { ContactInfo } from "../components/contact/ContactInfo";
 import { UserConsentPrivacyPolicyModal } from "../components/ui/UserConsentPrivacyPolicyModal";
 import { AppDownloadModal } from "../helper/AppDownloadModal";
-import { useEffect } from "react";
-import { useState } from "react";
 
-export const Home = () => {
-  const [modalDisplay, setModalDisplay] = useState(false);
-  const userAgent = navigator.userAgent;
-
+export const Home = () => { 
   return (
     <div className="general-container">
+      <AppDownloadModal />
       <Navbar />
-      {userAgent.indexOf("Android") !== -1 ? (
-        <AppDownloadModal
-          modalDisplay={modalDisplay}
-          setModalDisplay={setModalDisplay}
-        />
-      ) : (
-        ""
-      )}
-      {userAgent.indexOf("iPhone") !== -1 ||
-      userAgent.indexOf("iPad") !== -1 ? (
-        <AppDownloadModal
-          modalDisplay={modalDisplay}
-          setModalDisplay={setModalDisplay}
-        />
-      ) : (
-        ""
-      )}
       <ContactInfo />
       <UserConsentPrivacyPolicyModal />
     </div>
