37e1d7f7b0e0bc0ba3c13214b2414aeccb841a4d
grodriguezcontextglobal <grodriguez@contextglobal.com>
2023-01-02 15:05:04 -0500

    message box removed

--- src/page/admin/Admin.js
+++ src/page/admin/Admin.js
@@ -1,32 +1,13 @@
-import { useState } from "react";
 import { Navbar } from "../../components/admin/ui/Navbar";
 import "../../style/pages/admin/admin.css"
 
 export const Admin = () => {
-  const [message, setMessage] = useState('')
-
-  const sendNotifications = async(event) => {
-    event.preventDefault()
-    const notification = new Notification("New Notification", {
-      body:message,
-      timestamp: new Date(),
-      icon: "logo.jpg"
-    })
-    setMessage("")
-    return notification
-    
-  }
-
   return (
     <div>
       <Navbar />
-      <div className="notification-box">
-        <h3>Notification</h3>
-        <form className="notification-form" onSubmit={sendNotifications}>
-          <textarea name="message" value={message} onChange={event => setMessage(event.target.value)}/>
-          <button className="btn btn-create" type="submit">Send</button>
-        </form>
-      </div>
+      <h3>
+        Home Page
+      </h3>
     </div>
   );
 };
