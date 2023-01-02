df5d692964026993156be7513b739c9e206b4353
grodriguezcontextglobal <grodriguez@contextglobal.com>
2023-01-02 18:29:06 -0500

    message install app modal component imported

--- src/page/Home.js
+++ src/page/Home.js
@@ -2,11 +2,34 @@ import React from "react";
 import { Navbar } from "../components/ui/Navbar";
 import { ContactInfo } from "../components/contact/ContactInfo";
 import { UserConsentPrivacyPolicyModal } from "../components/ui/UserConsentPrivacyPolicyModal";
+import { AppDownloadModal } from "../helper/AppDownloadModal";
+import { useEffect } from "react";
+import { useState } from "react";
 
 export const Home = () => {
+  const [modalDisplay, setModalDisplay] = useState(false);
+  const userAgent = navigator.userAgent;
+
   return (
     <div className="general-container">
       <Navbar />
+      {userAgent.indexOf("Android") !== -1 ? (
+        <AppDownloadModal
+          modalDisplay={modalDisplay}
+          setModalDisplay={setModalDisplay}
+        />
+      ) : (
+        ""
+      )}
+      {userAgent.indexOf("iPhone") !== -1 ||
+      userAgent.indexOf("iPad") !== -1 ? (
+        <AppDownloadModal
+          modalDisplay={modalDisplay}
+          setModalDisplay={setModalDisplay}
+        />
+      ) : (
+        ""
+      )}
       <ContactInfo />
       <UserConsentPrivacyPolicyModal />
     </div>
