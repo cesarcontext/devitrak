ddda5dd9bf92f24cc68ecd59dbcfad4444e17c70
grodriguezcontextglobal <grodriguez@contextglobal.com>
2023-01-03 13:05:47 -0500

    relocate event

--- src/helper/AppDownloadModal.js
+++ src/helper/AppDownloadModal.js
@@ -4,6 +4,7 @@ import { Modal } from "react-bootstrap";
 
 export function AppDownloadModal({ onDownload, onClose }) {
   const [showModal, setShowModal] = useState(false);
+  const [hasPermission, setHasPermission] = useState(false);
   const userAgent = navigator.userAgent;
 
   const handleClose = () => setShowModal(false);
@@ -13,9 +14,47 @@ export function AppDownloadModal({ onDownload, onClose }) {
     }, 2000);
   }, []);
 
+  async function requestNotificationPermission() {
+  try {
+    const permission = await Notification.requestPermission();
+    // Permission granted
+    if (permission === "granted") {
+      return true;
+    } else {
+      // Permission denied
+      return false;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function checkPermission() {
+  const permission = await requestNotificationPermission();
+  setHasPermission(permission);
+}
+
+
   return (
     <>
-      {userAgent.indexOf("Android") !== -1 ? (
+          <Modal show={showModal} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Notifications?</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            The Devitrack app is now available for download. To install the app,
+            click the "Add to Home Screen" button in your browser's menu.
+            <br />
+            <br />
+            {checkPermission()}
+          </Modal.Body>
+          <Modal.Footer>
+            <button className="btn btn-delete" onClick={handleClose}>
+              Close
+            </button>
+          </Modal.Footer>
+        </Modal>
+      {/* {userAgent.indexOf("Android") !== -1 ? (
         <Modal show={showModal} onHide={handleClose}>
           <Modal.Header closeButton>
             <Modal.Title>App Download</Modal.Title>
@@ -75,7 +114,7 @@ export function AppDownloadModal({ onDownload, onClose }) {
         </Modal>
       ) : (
         ""
-      )}
+      )} */}
     </>
   );
 }
