347fddc9732cfe53e8e4b59e8debde20c13e465f
grodriguezcontextglobal <grodriguez@contextglobal.com>
2023-01-03 16:16:59 -0500

    condition to display modal if user has not requested a response

--- src/helper/AppDownloadModal.js
+++ src/helper/AppDownloadModal.js
@@ -4,52 +4,60 @@ import { Modal } from "react-bootstrap";
 
 export function AppDownloadModal({ onDownload, onClose }) {
   const [showModal, setShowModal] = useState(false);
-  const [hasPermission, setHasPermission] = useState(false);
+  let hasPermission = null;
   const userAgent = navigator.userAgent;
-
   const handleClose = () => setShowModal(false);
   useEffect(() => {
-    setTimeout(() => {
-      setShowModal(true);
-    }, 2000);
+   if(Notification.permission === "default"){
+     setTimeout(() => {
+        setShowModal(true);
+      }, 2000);
+   }
+     
+    
   }, []);
 
   async function requestNotificationPermission() {
-  try {
-    const permission = await Notification.requestPermission();
-    // Permission granted
-    if (permission === "granted") {
-      return setHasPermission(true);
-    } else {
-      // Permission denied
-      return setHasPermission(false);
+    try {
+      const permission = await Notification.requestPermission();
+      // Permission granted
+      if (permission === "granted") {
+        {
+          hasPermission = true;
+          setShowModal(false);
+        }
+      } else {
+        // Permission denied
+        {
+          hasPermission = false;
+          setShowModal(false);
+        }
+      }
+    } catch (error) {
+      console.error(error);
     }
-  } catch (error) {
-    console.error(error);
   }
-}
 
   return (
     <>
-          <Modal show={showModal} onHide={handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>Notifications?</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            The Devitrack app is now available for download. To install the app,
-            click the "Add to Home Screen" button in your browser's menu.
-            <br />
-            <br />
-          </Modal.Body>
-          <Modal.Footer>
-            <button className="btn btn-delete" onClick={handleClose}>
-              Close
-            </button>
-            <button className="btn btn-delete" onClick={()=> requestNotificationPermission()}>
-              Request Permission
-            </button>
-          </Modal.Footer>
-        </Modal>
+      <Modal show={showModal} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Notifications?</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          The Devitrack app wants to request your permission to send you
+          notifications.
+          <br />
+        </Modal.Body>
+        <Modal.Footer>
+          <button
+            className="btn btn-create"
+            onClick={() => requestNotificationPermission()}
+          >
+            Select Notification options
+          </button>
+        </Modal.Footer>
+      </Modal>
       {/* {userAgent.indexOf("Android") !== -1 ? (
         <Modal show={showModal} onHide={handleClose}>
           <Modal.Header closeButton>
