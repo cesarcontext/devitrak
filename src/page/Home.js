25f3e80df7f3c3ab78cbaf2671de15bf932dedd2
grodriguezcontextglobal <grodriguez@contextglobal.com>
2023-01-02 19:30:16 -0500

    function to detect if pwa app is installed already or not and display modal based on response

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React,{ useEffect, useState} from "react";
 import { Navbar } from "../components/ui/Navbar";
 import { ContactInfo } from "../components/contact/ContactInfo";
 import { UserConsentPrivacyPolicyModal } from "../components/ui/UserConsentPrivacyPolicyModal";
 import { AppDownloadModal } from "../helper/AppDownloadModal";
 
 export const Home = () => { 
+  const [isPwaInstalled, setIsPwaInstalled] = useState(false);
+  useEffect(() => {
+    const pwaInstalled = window.matchMedia('(display-mode: standalone)').matches;
+    setIsPwaInstalled(pwaInstalled);
+  }, []);
   return (
     <div className="general-container">
-      <AppDownloadModal />
+      {isPwaInstalled === false && <AppDownloadModal />}
       <Navbar />
       <ContactInfo />
       <UserConsentPrivacyPolicyModal />
