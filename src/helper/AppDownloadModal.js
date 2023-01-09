bd11e76e609d3d66da462837ac28ab7ee917329b
grodriguezcontextglobal <grodriguez@contextglobal.com>
2023-01-09 11:31:49 -0500

    comment removed

--- src/helper/AppDownloadModal.js
+++ src/helper/AppDownloadModal.js
@@ -2,10 +2,9 @@ import { useEffect } from "react";
 import { useState } from "react";
 import { Modal } from "react-bootstrap";
 
-export function AppDownloadModal({ onDownload, onClose }) {
+export function AppDownloadModal() {
   const [showModal, setShowModal] = useState(false);
   let hasPermission = null;
-  const userAgent = navigator.userAgent;
   const handleClose = () => setShowModal(false);
   useEffect(() => {
    if(Notification.permission === "default"){
@@ -20,14 +19,12 @@ export function AppDownloadModal({ onDownload, onClose }) {
   async function requestNotificationPermission() {
     try {
       const permission = await Notification.requestPermission();
-      // Permission granted
       if (permission === "granted") {
         {
           hasPermission = true;
           setShowModal(false);
         }
       } else {
-        // Permission denied
         {
           hasPermission = false;
           setShowModal(false);
@@ -58,83 +55,6 @@ export function AppDownloadModal({ onDownload, onClose }) {
           </button>
         </Modal.Footer>
       </Modal>
-      {/* {userAgent.indexOf("Android") !== -1 ? (
-        <Modal show={showModal} onHide={handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>App Download</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            The app is now available for download on Android. Click the links
-            below to download the app.
-            <br />
-          </Modal.Body>
-          <Modal.Footer>
-            <button className="btn btn-delete" onClick={handleClose}>
-              Close
-            </button>
-            <button
-              onClick={() => {
-                (event) => {
-                  window.addEventListener("beforeinstallprompt", () => {
-                    event.preventDefault();
-                    const promptEvent = event;
-                    promptEvent.prompt();
-                    promptEvent.userChoice.then((choiceResult) => {
-                      if (choiceResult.outcome === "accepted") {
-                        console.log("App was installed");
-                      } else {
-                        console.log("App was not installed");
-                      }
-                    });
-                  });
-                };
-              }}
-              className="btn btn-create"
-            >
-              DownLoad App
-            </button>{" "}
-          </Modal.Footer>
-        </Modal>
-      ) : (
-        ""
-      )}
-      {userAgent.indexOf("iPhone") !== -1 ||
-      userAgent.indexOf("iPad") !== -1 ? (
-        <Modal show={showModal} onHide={handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>App Download</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            The Devitrack app is now available for download. To install the app,
-            click the "Add to Home Screen" button in your browser's menu.
-            <br />
-            <br />
-          </Modal.Body>
-          <Modal.Footer>
-            <button className="btn btn-delete" onClick={handleClose}>
-              Close
-            </button>
-          </Modal.Footer>
-        </Modal>
-      ) : (
-        ""
-      )} */}
     </>
   );
 }
-/**
- * (event) => {
-              window.addEventListener("beforeinstallprompt", () => {
-                event.preventDefault();
-                const promptEvent = event;
-                promptEvent.prompt();
-                promptEvent.userChoice.then((choiceResult) => {
-                  if (choiceResult.outcome === "accepted") {
-                    console.log("App was installed");
-                  } else {
-                    console.log("App was not installed");
-                  }
-                });
-              });
-            }}
- */
