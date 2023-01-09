8d65046a337aeb212528b4978f56c008263e18b3
grodriguezcontextglobal <grodriguez@contextglobal.com>
2023-01-09 13:10:52 -0500

    replace single variable for usestate

--- src/helper/AppDownloadModal.js
+++ src/helper/AppDownloadModal.js
@@ -4,31 +4,25 @@ import { Modal } from "react-bootstrap";
 
 export function AppDownloadModal() {
   const [showModal, setShowModal] = useState(false);
-  let hasPermission = null;
+  const [hasPermission, setHasPermission] = useState(null)
   const handleClose = () => setShowModal(false);
   useEffect(() => {
-   if(Notification.permission === "default"){
-     setTimeout(() => {
+    if (Notification.permission === "default") {
+      setTimeout(() => {
         setShowModal(true);
       }, 2000);
-   }
-     
-    
+    }
   }, []);
 
   async function requestNotificationPermission() {
     try {
       const permission = await Notification.requestPermission();
       if (permission === "granted") {
-        {
-          hasPermission = true;
-          setShowModal(false);
-        }
+        setHasPermission(true);
+        setShowModal(false);
       } else {
-        {
-          hasPermission = false;
-          setShowModal(false);
-        }
+        setHasPermission(false);
+        setShowModal(false);
       }
     } catch (error) {
       console.error(error);
