65666266ce7bac3c996d0ebaadc932abfff650f8
grodriguezcontextglobal <grodriguez@contextglobal.com>
2023-01-09 10:19:10 -0500

    image added to aware user to redirect to browser

--- src/components/passwordless/MagicLink.js
+++ src/components/passwordless/MagicLink.js
@@ -3,7 +3,6 @@ import { rightDoneMessage } from "../../helper/swalFireMessage";
 import "../../style/component/passwrodless/MagicLink.css";
 
 export const MagicLink = (magicLinkParam) => {
-  
   const client = useStytch();
 
   const emailUserToPassToMagicLink = Object.values(magicLinkParam);
@@ -17,11 +16,21 @@ export const MagicLink = (magicLinkParam) => {
   };
   return (
     <div className="email-sent-message-magic-link">
-      
       <p onClick={handleLogin}>
-      <i className="bi bi-exclamation-diamond" />  Your email is already in the system. Please click this link to log in
-        through your email inbox <i className="bi bi-chevron-right" />
+        <i className="bi bi-exclamation-diamond" /> Your email is already in the
+        system. Please click this link to log in through your email inbox{" "}
+        <i className="bi bi-chevron-right" />
       </p>
+      <div>
+        <p>
+          Please, sometimes for security set up of the device, it should need to
+          redirect to default browser.
+        </p>
+        <img
+          src={require("../../image/screen-alert-magic-link.png")}
+          alt="screen alert"
+        />
+      </div>
     </div>
   );
 };
