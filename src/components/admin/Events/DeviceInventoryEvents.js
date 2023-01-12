9374db283355b3ef4c57f45cba7d9534768eba22
grodriguezcontextglobal <grodriguez@contextglobal.com>
2023-01-11 20:58:57 -0500

    events components split

---
+++ src/components/admin/Events/DeviceInventoryEvents.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react'
+import { useState } from 'react';
+import { devitrackApi } from "../../../apis/devitrackApi";
+import { GraphicDisplayDevices } from './GraphicDisplayDevices';
+
+export const DeviceInventoryEvents = () => {
+    const [listOfReceiver, setListOfReceiver] = useState([]);
+
+    useEffect(() => {
+      const callApi = async () => {
+        const response = await devitrackApi.get("/receiver/receiver-pool-list");
+        if (response) {
+          setListOfReceiver(response.data.receiversInventory);
+        }
+      };
+      return () => {
+        callApi();
+      };
+    }, []);
+
+    return (
+        <div className="container-live-events">
+          <h5>Devices Inventory</h5>
+          <p>{`There are `}<strong>{`${listOfReceiver.length}`}</strong>{` devices to be returned from all event.`}</p>
+          <GraphicDisplayDevices />
+        </div>
+      );
+    };
