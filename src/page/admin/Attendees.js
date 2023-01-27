0e25ce51ffb1a5547a7546d595c4e182079ae4e5
grodriguezcontextglobal <grodriguez@contextglobal.com>
2023-01-26 20:11:56 -0500

    condition search redefined function

--- src/page/admin/Attendees.js
+++ src/page/admin/Attendees.js
@@ -1,12 +1,17 @@
 import React, { useState } from "react";
 import { AttendeesInfo } from "../../components/admin/Attendees/AttendeesInfo";
+import { SearchAttendeesByEmail } from "../../components/admin/Attendees/SearchAttendeesByEmail";
 import { StripeTransactionHistoryByUser } from "../../components/admin/Attendees/StripeTransactionHistoryByUser";
 import { Navbar } from "../../components/admin/ui/Navbar";
 import { ResultBySearch } from "../../helper/ResultBySearch";
-import "../../style/pages/admin/attendees.css"
+import "../../style/pages/admin/attendees.css";
 
 export const Attendees = () => {
   const [searchTerm, setSearchTerm] = useState("");
+  console.log(
+    "🚀 ~ file: Attendees.js:45 ~ Attendees ~ searchTerm",
+    searchTerm
+  );
 
   return (
     <div>
@@ -19,17 +24,24 @@ export const Attendees = () => {
             </div>
             <div className="search-input">
               <input
-              className="search-input-field"
+                className="search-input-field"
                 name="value"
                 onChange={(e) => setSearchTerm(e.target.value)}
                 value={searchTerm}
                 placeholder="Search"
               />
-              <i onClick={() => setSearchTerm("")} id="icon-delete-searchTerm" className="bi bi-x"/>
+              {searchTerm !== "" && (
+                <i
+                  onClick={() => setSearchTerm("")}
+                  id="icon-delete-searchTerm"
+                  className="bi bi-x"
+                />
+              )}
             </div>
           </div>
+          <div>{searchTerm === "" && <AttendeesInfo />}</div>
           <div>
-            {searchTerm === "" ? <AttendeesInfo/> : <ResultBySearch searchTerm={searchTerm} />}
+            {searchTerm !== "" && <ResultBySearch searchTerm={searchTerm} />}
           </div>
           <div className="d-none">
             {" "}
