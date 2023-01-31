1785e0c846b734797ba030e586e85156f81ec9dc
grodriguezcontextglobal <grodriguez@contextglobal.com>
2023-01-30 22:45:44 -0500

    unnecessary imports removeed. New file imported

--- src/routes/AdminRoutes.js
+++ src/routes/AdminRoutes.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 import { Routes, Route } from "react-router";
 import { useSelector } from "react-redux";
-import { Admin } from "../page/admin/Admin";
+// import { Admin } from "../page/admin/Admin";
 import { ArticleContentCreation } from "../components/admin/Articles/ArticleContentCreation";
 import { Articles } from "../page/admin/Articles";
 import { Attendees } from "../page/admin/Attendees";
@@ -11,7 +11,10 @@ import { LoginRegisterAdmin } from "../page/admin/LoginAdmin";
 import { Profile } from "../page/admin/Profile";
 import { ReceiversDetailsAssignation } from "../components/admin/Attendees/ReceiversDetailsAssignation";
 import { Settings } from "../page/admin/Settings";
-import { TriggerNotification } from "../components/admin/Notification/TriggerNotification";
+import { Events } from "../page/admin/Events";
+import { NoticeTransactionWentTrue } from "../page/admin/NoticeTransactionWentTrue";
+import { DeviceInUse } from "../page/admin/DeviceInUse";
+import { DetailUser } from "../components/admin/Attendees/DetailUser";
 
 export const AdminRoutes = () => {
   const { status } = useSelector((state) => state.admin);
@@ -21,11 +24,13 @@ export const AdminRoutes = () => {
       <Routes>
         {status === "authenticated" && adminToken ? (
           <>
-            <Route path="/admin" element={<Admin />}></Route>
+            {/* <Route path="/admin" element={<Admin />}></Route> */}
             <Route path="/admin/device-database" element={<DeviceDatabase />} />
             <Route path="/admin/articles" element={<Articles />} />
             <Route path="/admin/settings" element={<Settings />} />
             <Route path="/admin/attendees" element={<Attendees />} />
+            <Route path="/admin/attendee/:id" element={<DetailUser />} />
+            <Route path="/admin/events" element={<Events />} />
             <Route
               path="/admin/attendees/receiver_assignation"
               element={<ReceiversDetailsAssignation />}
@@ -34,10 +39,19 @@ export const AdminRoutes = () => {
               path="/admin/create-article"
               element={<ArticleContentCreation />}
             />
+            <Route
+              path="/admin/device-database/device-in-use"
+              element={<DeviceInUse />}
+            />
+
             <Route path="/admin/profile" element={<Profile />} />
             <Route
               path="/admin/*"
-              element={<Navigate to="/admin/" replace />}
+              element={<Navigate to="/admin/events" replace />}
+            />
+            <Route
+              path="/admin/transaction-confirmed"
+              element={<NoticeTransactionWentTrue />}
             />
           </>
         ) : (
@@ -50,15 +64,6 @@ export const AdminRoutes = () => {
           </>
         )}
       </Routes>
-      {window.location.pathname === "/admin/settings" && (
-        <TriggerNotification />
-      )}
-      {window.location.pathname === "/admin/attendees" && (
-        <TriggerNotification />
-      )}
-      {window.location.pathname === "/admin/device-database" && (
-        <TriggerNotification />
-      )}
     </div>
   );
 };
