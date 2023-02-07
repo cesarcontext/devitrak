87fc14fdf91fed6229c1b700eed59adf9c96a197
grodriguezcontextglobal <grodriguez@contextglobal.com>
2023-02-06 19:04:34 -0500

    word changed to authorize instead of deposit

--- src/components/stripe/StripeCheckForm.js
+++ src/components/stripe/StripeCheckForm.js
@@ -108,7 +108,7 @@ export const StripeCheckoutForm = () => {
         id="submit"
       >
         <span id="button-text">
-          {isLoading ? <div className="spinner" id="spinner"></div> : `Deposit $${device * 200}`}
+          {isLoading ? <div className="spinner" id="spinner"></div> : `Authorize $${device * 200}`}
         </span>
       </button>
       {message && <div id="payment-message">{message}</div>}
