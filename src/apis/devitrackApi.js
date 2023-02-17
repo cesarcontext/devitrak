0ab1a8f34d546695103af95b413c1989ed49917e
grodriguezcontextglobal <grodriguez@contextglobal.com>
2023-02-17 16:12:19 -0500

    added new backend link

--- src/apis/devitrackApi.js
+++ src/apis/devitrackApi.js
@@ -1,13 +1,30 @@
+/**
+
+Axios instance for accessing Devitrack API.
+@type {Object}
+@property {Object} devitrackApi - Instance for general API requests.
+@property {Object} devitrackApiAdmin - Instance for API requests with admin access.
+@property {Object} devitrackApiStripe - Instance for API requests related to Stripe.
+@property {Object} devitrackApiArticle - Instance for API requests related to articles.
+*/
 import axios from "axios";
 
 export const devitrackApi = axios.create({
-  baseURL: process.env.REACT_APP_URL,
+  baseURL: 'https://db.devitrak.net/',
 });
 
+
 export const devitrackApiAdmin = axios.create({
-  baseURL: `${process.env.REACT_APP_URL}/admin`
+  baseURL: `https://db.devitrak.net/admin`
 })
 
+/**
+
+Configures interceptors for requests to the devitrackApiAdmin instance.
+@function
+@param {Object} config - The configuration object for the request.
+@returns {Object} - The modified configuration object with authentication headers if the user is an admin.
+*/
 devitrackApiAdmin.interceptors.request.use((config) => {
   if (localStorage.getItem("admin-token")) {
     config.headers = {
@@ -18,9 +35,16 @@ devitrackApiAdmin.interceptors.request.use((config) => {
 });
 
 export const devitrackApiStripe = axios.create({
-  baseURL: `${process.env.REACT_APP_URL}/stripe`
+  baseURL: `https://db.devitrak.net/stripe`
 })
 
+/**
+
+Configures interceptors for requests to the devitrackApiStripe instance.
+@function
+@param {Object} config - The configuration object for the request.
+@returns {Object} - The modified configuration object with authentication headers if the user has a token.
+*/
 //TODO: config interceptors
 devitrackApiStripe.interceptors.request.use((config) => {
   if (localStorage.getItem("token")) {
@@ -32,8 +56,16 @@ devitrackApiStripe.interceptors.request.use((config) => {
 });
 
 export const devitrackApiArticle = axios.create({
-  baseURL: `${process.env.REACT_APP_URL}/article`
+  baseURL: `https://db.devitrak.net/article`
 })
+/**
+
+Configures interceptors for requests to the devitrackApiArticle instance.
+@function
+@param {Object} config - The configuration object for the request.
+@returns {Object} - The modified configuration object with authentication headers if the user is an admin.
+*/
+
 
 //TODO: config interceptors
 devitrackApiArticle.interceptors.request.use((config) => {
