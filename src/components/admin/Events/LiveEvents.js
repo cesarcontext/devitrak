6b8a8a7ddef3dad049660f35d741e6ab73b88cde
grodriguezcontextglobal <grodriguez@contextglobal.com>
2023-02-23 20:02:50 -0500

    conditions added to sort list based on date

--- src/components/admin/Events/LiveEvents.js
+++ src/components/admin/Events/LiveEvents.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { devitrackApi } from "../../../apis/devitrackApi";
-import "../../../style/component/events/live-events.css"
+import { eventList } from "../json/eventList";
+import "../../../style/component/admin/events/live-events.css";
 
 export const LiveEvents = () => {
   const [listOfReceiver, setListOfReceiver] = useState([]);
@@ -16,24 +17,55 @@ export const LiveEvents = () => {
       callApi();
     };
   }, []);
+  const currentDate = new Date();
 
   return (
     <div className="container-live-events">
       <h5>Live Events</h5>
       <table className="table">
         <thead>
-          <tr style={{borderBottom:"transparant", marginBottom:"5px"}}>
+          <tr style={{ borderBottom: "transparant", marginBottom: "5px" }}>
             <th scope="col">EVENT NAME</th>
             <th scope="col">EVENT LOCATION</th>
             <th scope="col">DEVICES AMOUNT</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>National Retail Conference</td>
-            <td>New York, New York</td>
-            <td>{listOfReceiver.length}</td>
-          </tr>
+          {eventList.map((location) => {
+            console.log(
+              "🚀 ~ file: LiveEvents.js:35 ~ {eventList.map ~ location:",
+              location
+            );
+            let checkArrayLength = new Map();
+            listOfReceiver.map((item) => {
+              console.log(
+                "🚀 ~ file: LiveEvents.js:38 ~ listOfReceiver.map ~ item:",
+                item
+              );
+              if (item.eventSelected === location.name) {
+                return checkArrayLength.set(item.device, item);
+              }
+              return null;
+            });
+            if (
+              location.scheduleBeging >= currentDate &&
+              location.scheduleEnd <= currentDate
+            ) {
+              return (
+                <tr
+                  style={{
+                    cursor: "pointer",
+                  }}
+                  onClick
+                >
+                  <td>{location.name}</td>
+                  <td>{location.location}</td>
+                  <td>{checkArrayLength.size}</td>
+                </tr>
+              );
+            }
+            return null;
+          })}
         </tbody>
       </table>
     </div>
--- src/components/admin/Events/PastEvents.js
+++ src/components/admin/Events/PastEvents.js
@@ -1,25 +1,74 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { devitrackApi } from "../../../apis/devitrackApi";
+import "../../../style/component/admin/events/live-events.css"
+import { eventList } from "../json/eventList";
 
 export const PastEvents = () => {
-    return (
-        <div className="container-live-events">
-          <h5>Pasts Events</h5>
-          <table className="table">
-            <thead>
-              <tr style={{borderBottom:"transparant", marginBottom:"5px"}}>
-                <th scope="col">EVENT NAME</th>
-                <th scope="col">EVENT LOCATION</th>
-                <th scope="col">DEVICES AMOUNT</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>National Retail Conference</td>
-                <td>New York, New York</td>
-                <td></td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      );
+  const [listOfReceiver, setListOfReceiver] = useState([]);
+
+  useEffect(() => {
+    const callApi = async () => {
+      const response = await devitrackApi.get("/receiver/receiver-pool-list");
+      if (response) {
+        setListOfReceiver(response.data.receiversInventory);
+      }
     };
+    return () => {
+      callApi();
+    };
+  }, []);
+
+  const array = {}
+  for (let data of eventList){
+    if(!array[data.name]){
+      return array[data.name] = data
+    }
+  }
+  console.log("checking", array)
+  
+  const currentDate = new Date();
+
+  return (
+    <div className="container-live-events">
+      <h5>Past Events</h5>
+      <table className="table">
+        <thead>
+          <tr style={{borderBottom:"transparant", marginBottom:"5px"}}>
+            <th scope="col">EVENT NAME</th>
+            <th scope="col">EVENT LOCATION</th>
+            <th scope="col">DEVICES AMOUNT</th>
+          </tr>
+        </thead>
+        <tbody>
+        {eventList.map((location) => {
+          let arrayToCheck = []
+          for( let data of listOfReceiver ){
+            if(location.name === data.eventSelected){
+              arrayToCheck.push(data)
+            }
+          }
+            if (
+              location.scheduleBeging >= currentDate &&
+              location.scheduleEnd <= currentDate
+            ) {
+              return (
+                <tr
+                  style={{
+                    cursor: "pointer",
+                  }}
+                  onClick
+                >
+                  <td>{location.name}</td>
+                  <td>{location.location}</td>
+                  <td>{arrayToCheck.length}</td>
+                </tr>
+              );
+            }
+            return null;
+          })}
+      
+        </tbody>
+      </table>
+    </div>
+  );
+};
