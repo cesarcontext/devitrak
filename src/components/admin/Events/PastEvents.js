1e3b5e97f5af65338d2f8baf967870b1119711be
grodriguezcontextglobal <grodriguez@contextglobal.com>
2023-03-06 00:24:58 -0500

    function to check if event happened and render it

--- src/components/admin/Events/PastEvents.js
+++ src/components/admin/Events/PastEvents.js
@@ -1,23 +1,49 @@
+import { func } from "prop-types";
 import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import { devitrackApi } from "../../../apis/devitrackApi";
-import { eventList } from "../../../json/eventList";
 import "../../../style/component/admin/events/live-events.css";
 
 export const PastEvents = () => {
   const [listOfReceiver, setListOfReceiver] = useState([]);
+  const [events, setEvents] = useState([]);
+  const { user } = useSelector((state) => state.admin);
   const currentDate = new Date();
   useEffect(() => {
     const callApi = async () => {
-      const response = await devitrackApi.get("/receiver/receiver-pool-list");
+      const response = await devitrackApi.get(
+        "/receiver/receiver-assigned-list"
+      );
       if (response) {
-        setListOfReceiver(response.data.receiversInventory);
+        setListOfReceiver(response.data.listOfReceivers);
       }
     };
-    return () => {
-      callApi();
-    };
-  }, []);
+    callApi();
+  }, [listOfReceiver]);
 
+  useEffect(() => {
+    const callApiEvent = async () => {
+      const response = await devitrackApi.get("/event/event-list");
+      if (response) {
+        setEvents(response.data.list);
+      }
+    };
+    callApiEvent();
+  }, [events]);
+
+  const sortArray = () => {
+    let deviceSum = [];
+    for (let location of events) {
+      listOfReceiver.map((event, index) => {
+        if (location.eventName === event.eventSelected) {
+          deviceSum.push(event.device.length);
+        }
+      });
+      return deviceSum.reduce(function (a, b) {
+        return a + b;
+      }, 0);
+    }
+  };
   return (
     <div className="container-live-events">
       <h5>Past Events</h5>
@@ -30,21 +56,21 @@ export const PastEvents = () => {
           </tr>
         </thead>
         <tbody>
-          {eventList.map((location) => {
-            if (currentDate > location.scheduleEnd) {
+          {events.map((location) => {
+            const check = new Date(`${location.dateEnd}`);
+            if (currentDate > check && location.company === user.company) {
               return (
-                <>
-                  <tr
-                    style={{
-                      cursor: "pointer",
-                    }}
-                    onClick
-                  >
-                    <td>{location.name}</td>
-                    <td>{location.location}</td>
-                    <td>{listOfReceiver.length}</td>
-                  </tr>{" "}
-                </>
+                <tr
+                key={location.id}
+                  style={{
+                    cursor: "pointer",
+                  }}
+                  onClick
+                >
+                  <td>{location.eventName}</td>
+                  <td>{location.location}</td>
+                  <td>{sortArray()}</td>
+                </tr>
               );
             }
             return null;
