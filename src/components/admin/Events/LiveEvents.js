8119e9104e16a2b5ba23c66dde6f7b2202b9623e
grodriguezcontextglobal <grodriguez@contextglobal.com>
2023-03-07 16:29:31 -0500

    navigate routes actions / function to assign event

--- src/components/admin/Events/LiveEvents.js
+++ src/components/admin/Events/LiveEvents.js
@@ -1,10 +1,16 @@
 import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router";
 import { devitrackApi } from "../../../apis/devitrackApi";
-import { eventList } from "../../../json/eventList";
+import { onSelectEvent } from "../../../store/slices/eventSlice";
 import "../../../style/component/admin/events/live-events.css";
 
 export const LiveEvents = () => {
   const [listOfReceiver, setListOfReceiver] = useState([]);
+  const [events, setEvents] = useState([]);
+  const { user } = useSelector((state) => state.admin);
+  const dispatch = useDispatch();
+  const navigate = useNavigate()
   const currentDate = new Date();
   useEffect(() => {
     const callApi = async () => {
@@ -13,11 +19,23 @@ export const LiveEvents = () => {
         setListOfReceiver(response.data.receiversInventory);
       }
     };
-    return () => {
-      callApi();
-    };
-  }, []);
+    callApi();
+  }, [listOfReceiver]);
 
+  useEffect(() => {
+    const callApiEvent = async () => {
+      const response = await devitrackApi.get("/event/event-list");
+      if (response) {
+        setEvents(response.data.list);
+      }
+    };
+    callApiEvent();
+  }, [events]);
+
+  const handleEventSelected = (location) => {
+    dispatch(onSelectEvent(location.eventName))
+    navigate("/admin/attendees")
+  };
   return (
     <div className="container-live-events">
       <h5>Live Events</h5>
@@ -26,25 +44,26 @@ export const LiveEvents = () => {
           <tr style={{ borderBottom: "transparant", marginBottom: "5px" }}>
             <th scope="col">EVENT NAME</th>
             <th scope="col">EVENT LOCATION</th>
-            <th scope="col">DEVICES AMOUNT</th>
+            {/* <th scope="col">DEVICES AMOUNT</th> */}
           </tr>
         </thead>
         <tbody>
-          {eventList.map((location) => {
+          {events.map((location) => {
             if (
-              location.scheduleBeging >= currentDate &&
-              location.scheduleEnd <= currentDate
+              location.dateBegin >= currentDate &&
+              location.dateEnd <= currentDate &&
+              location.company === user.company
             ) {
               return (
                 <tr
                   style={{
                     cursor: "pointer",
                   }}
-                  onClick
+                  onClick={async () => handleEventSelected(location)}
                 >
                   <td>{location.name}</td>
                   <td>{location.location}</td>
-                  <td>{listOfReceiver.length}</td>
+                  {/* <td>{listOfReceiver.length}</td> */}
                 </tr>
               );
             }
