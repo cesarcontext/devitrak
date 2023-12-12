125fe03d25a1cd4d4b90f16909af00f942ba8ffe
grodriguezcontextglobal <grodriguez@contextglobal.com>
2023-12-11 23:20:23 -0500

    redefining functions to store payment intent

--- src/pages/display-qr-code/DisplayQRCode.jsx
+++ src/pages/display-qr-code/DisplayQRCode.jsx
@@ -1,11 +1,11 @@
+import { Grid, Typography } from "@mui/material";
 import { nanoid } from "@reduxjs/toolkit";
 import { Avatar, Divider, List, QRCode } from "antd";
-import React, { useCallback, useEffect, useRef, useState } from "react";
-import { useSelector } from "react-redux";
-import { devitrackApi } from "../../devitrakApi";
-import { Grid, Typography } from "@mui/material";
+import React, { useCallback, useRef, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-
+import { devitrackApi } from "../../devitrakApi";
+import { onAddPaymentIntent } from "../../store/slides/stripeSlide";
 const DisplayQRCode = () => {
   const { consumer } = useSelector((state) => state.consumer);
   const { currentOrder } = useSelector((state) => state.deviceHandler);
@@ -13,195 +13,263 @@ const DisplayQRCode = () => {
   const [qrCodeValue, setQrCodeValue] = useState(undefined);
   const paymentIntentValueRef = useRef(null);
   const nanoGenerated = useRef(false);
+  const storingRef = useRef(true);
+  const transactionRef = useRef(true);
+  const count = useRef(0)
+  const propsToPass = useRef({
+    paymentIntentGenerated: "pi_" + nanoid(12),
+    clientSecretGenerated: `_client_secret=${nanoid(12)}`,
+  });
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const _ = require("lodash");
+  const payment_intent = new URLSearchParams(window.location.search).get(
+    "payment_intent"
+  );
+  const clientSecret = new URLSearchParams(window.location.search).get(
+    "payment_intent_client_secret"
+  );
+
   const foundTotalDeviceNumber = () => {
     const number = currentOrder?.map((total) => parseInt(total.deviceNeeded));
     return number.reduce((accumulator, current) => accumulator + current, 0);
   };
   foundTotalDeviceNumber();
-  const generatePaymentIntentForNoDepositRequired = useCallback(
-    async (props) => {
-      const stripeTransactionProfile = {
-        paymentIntent: props.paymentIntentGenerated,
-        clientSecret: props.clientSecretGenerated,
-        device: foundTotalDeviceNumber(),
-        user: consumer.id,
-        provider: company,
-        eventSelected: choice,
-      };
-      await devitrackApi.post(
-        "/stripe/stripe-transaction-no-regular-user",
-        stripeTransactionProfile
-      );
-    },
-    []
-  );
-  const generateTransactionInfoDetail = useCallback(async (props) => {
-    const transactionProfile = {
-      paymentIntent: props.paymentIntentGenerated,
-      clientSecret: props.clientSecretGenerated,
-      device: currentOrder,
-      consumerInfo: consumer,
+
+  const storeStripePaymentIntent = useCallback(async () => {
+    const stripeTransactionProfile = {
+      paymentIntent: payment_intent,
+      clientSecret: clientSecret,
+      device: foundTotalDeviceNumber(),
+      user: consumer.id,
       provider: company,
       eventSelected: choice,
-      date: new Date(),
     };
-    await devitrackApi.post("/stripe/save-transaction", transactionProfile);
+    await devitrackApi.post(
+      "/stripe/stripe-transaction",
+      stripeTransactionProfile
+    );
   }, []);
 
-  const propsToPass = {
-    paymentIntentGenerated: 0,
-    clientSecretGenerated: 0,
+  const generatePaymentIntentForNoDepositRequired = useCallback(async () => {
+    const stripeTransactionProfile = {
+      paymentIntent: propsToPass.current.paymentIntentGenerated,
+      clientSecret: propsToPass.current.clientSecretGenerated,
+      device: foundTotalDeviceNumber(),
+      user: consumer.id,
+      provider: company,
+      eventSelected: choice,
+    };
+    await devitrackApi.post(
+      "/stripe/stripe-transaction-no-regular-user",
+      stripeTransactionProfile
+    );
+  }, []);
+
+  const generateTransactionInfoDetail = useCallback(async () => {
+    if (transactionRef.current) {
+      const transactionProfile = {
+        paymentIntent: propsToPass.current.paymentIntentGenerated,
+        clientSecret: propsToPass.current.clientSecretGenerated,
+        device: currentOrder[0],
+        consumerInfo: consumer,
+        provider: company,
+        eventSelected: choice,
+        date: new Date(),
+      };
+      await devitrackApi.post("/stripe/save-transaction", transactionProfile);
+      return (transactionRef.current = false);
+    }
+  }, []);
+
+  const generator = useCallback(async () => {
+    if (nanoGenerated.current === false) {
+      setQrCodeValue(propsToPass.current.paymentIntentGenerated);
+      paymentIntentValueRef.current = propsToPass.current.paymentIntentGenerated;
+      if (count.current === 0) {
+        generatePaymentIntentForNoDepositRequired();
+        generateTransactionInfoDetail()
+        count.current = 1
+      };
+      return nanoGenerated.current = true;
+    }
+  }, [])
+
+  if (payment_intent && clientSecret) {
+    if (storingRef.current) {
+      propsToPass.current = {
+        paymentIntentGenerated: payment_intent,
+        clientSecretGenerated: clientSecret,
+      };
+      storeStripePaymentIntent();
+      generateTransactionInfoDetail(propsToPass);
+      setQrCodeValue(payment_intent);
+      storingRef.current = false;
+    }
+  } else {
+    generator()
+  }
+
+  const checkAndRemove = async () => {
+    const savedStripeTransactions = await devitrackApi.get("/admin/users");
+    const savedTransactions = await devitrackApi.get("/stripe/transaction");
+    const groupingStripeTransactionByEvent = _.groupBy(
+      savedStripeTransactions.data.stripeTransactions,
+      "eventSelected"
+    );
+    const refDataStripeTransaction = Object.values(
+      groupingStripeTransactionByEvent[choice]
+    );
+    const groupingTransactionByEvent = _.groupBy(
+      savedTransactions.data.list,
+      "eventSelected"
+    );
+
+    const detectingStripePaymentIntentDuplicated = {};
+    for (let data of refDataStripeTransaction) {
+      if (!detectingStripePaymentIntentDuplicated[data.paymentIntent]) {
+        detectingStripePaymentIntentDuplicated[data.paymentIntent] = 1;
+      } else {
+        await devitrackApi.delete(`/stripe/remove-duplicate/${data.id}`);
+      }
+    }
+    const refDataTransaction = Object.values(
+      groupingTransactionByEvent[choice]
+    );
+    const detectingTransactionDuplicated = {};
+    for (let data of refDataTransaction) {
+      if (!detectingTransactionDuplicated[data.paymentIntent]) {
+        detectingTransactionDuplicated[data.paymentIntent] = 1;
+      } else {
+        await devitrackApi.delete(`/stripe/removing/${data.id}`);
+      }
+    }
   };
+  checkAndRemove();
 
-  useEffect(() => {
-    const controller = new AbortController();
-    const generator = async () => {
-      if (nanoGenerated.current === false) {
-        propsToPass.paymentIntentGenerated = "pi_" + nanoid(12);
-        propsToPass.clientSecretGenerated = `_client_secret=${nanoid(12)}`;
-        nanoGenerated.current = true;
-      }
-      paymentIntentValueRef.current = propsToPass.paymentIntentGenerated;
-      await generatePaymentIntentForNoDepositRequired(propsToPass);
-      await generateTransactionInfoDetail(propsToPass);
-    };
-    generator();
-    const checkAndRemove = async () => {
-      const savedStripeTransactions = await devitrackApi.get("/admin/users");
-      const savedTransactions = await devitrackApi.get("/stripe/transaction");
-      const check =
-        await savedStripeTransactions.data.stripeTransactions.filter(
-          (transaction) =>
-            transaction.paymentIntent === propsToPass.paymentIntentGenerated
-        );
-      const checkTransactions = await savedTransactions.data.list.filter(
-        (transaction) =>
-          transaction.paymentIntent === propsToPass.paymentIntentGenerated
-      );
-      if (check.length > 1 && checkTransactions.length > 1) {
-        setQrCodeValue(check.at(0));
-        devitrackApi.delete(`/stripe/remove-duplicate/${check.at(0).id}`);
-        devitrackApi.delete(
-          `/transaction/remove-duplicate-transaction/${
-            checkTransactions.at(0).id
-          }`
-        );
-      }
-    };
-    checkAndRemove();
-
-    return () => {
-      controller.abort();
-    };
-  }, []); // eslint-disable-line no-use-before-define
   const automaticNavigation = () => {
     setTimeout(() => {
+      dispatch(onAddPaymentIntent(undefined));
       return navigate("/device", { replace: true });
     }, 2000);
-    return null
+    return null;
   };
 
   return (
-    <>
-      <Grid
-        display={"flex"}
-        flexDirection={"column"}
-        justifyContent={"center"}
-        alignItems={"center"}
-        container
-      >
-        <Grid marginY={5} item xs={12}>
-          <Typography
-            textTransform={"none"}
-            color={"var(--gray-900, #101828)"}
-            textAlign={"center"}
-            /* Display xs/Semibold */
-            fontFamily={"Inter"}
-            fontSize={"18px"}
-            fontStyle={"normal"}
-            fontWeight={400}
-            lineHeight={"24px"}
-            style={{
-              textWrap: "balance",
-            }}
-          >
-            We are taking you to the current order page for more information.
-            {automaticNavigation()}
-          </Typography>
-        </Grid>
-        <Grid item xs={12}>
-          <QRCode
-            bgColor="#fff"
-            errorLevel="H"
-            value={
-              qrCodeValue ? qrCodeValue.paymentIntent : "https://devitrak.com"
-            }
-            icon="https://i.ibb.co/kKktFyw/maskable-icon.png"
-          />
-        </Grid>
-        <Grid margin={"1rem auto"} item xs={12}>
-          <Typography
-            textTransform={"none"}
-            color={"var(--gray-900, #101828)"}
-            textAlign={"left"}
-            /* Display xs/Semibold */
-            fontFamily={"Inter"}
-            fontSize={"24px"}
-            fontStyle={"normal"}
-            fontWeight={600}
-            lineHeight={"32px"}
-          >
-            Order completed
-          </Typography>
-        </Grid>
-        <Grid marginX={"auto"} item xs={12}>
-          {" "}
-          <Typography
-            textTransform={"none"}
-            color={"var(--gray-900, #101828)"}
-            textAlign={"center"}
-            /* Display xs/Semibold */
-            fontFamily={"Inter"}
-            fontSize={"14px"}
-            fontStyle={"normal"}
-            fontWeight={400}
-            lineHeight={"20px"}
-            padding={"auto"}
-            style={{
-              textWrap: "balance",
-              width: "80%",
-              margin: "0 auto",
-            }}
-          >
-            Please proceed to go to the pick up location to collect your{" "}
-            {foundTotalDeviceNumber() > 1 ? "devices" : "device"}. Show this qr
-            code to staff to find your order.
-          </Typography>
-        </Grid>
-
-        <Divider />
-        <List
+    <Grid
+      display={"flex"}
+      flexDirection={"column"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      container
+    >
+      <Grid marginY={5} item xs={12}>
+        <Typography
+          textTransform={"none"}
+          color={"var(--gray-900, #101828)"}
+          textAlign={"center"}
+          /* Display xs/Semibold */
+          fontFamily={"Inter"}
+          fontSize={"18px"}
+          fontStyle={"normal"}
+          fontWeight={400}
+          lineHeight={"24px"}
           style={{
-            width: "90%",
+            textWrap: "balance",
           }}
-          itemLayout="horizontal"
-          dataSource={currentOrder}
-          renderItem={(item) => (
-            <List.Item
-              actions={[<p key="list-loadmore-more">{item.deviceNeeded}</p>]}
-            >
-              <List.Item.Meta
-                avatar={<Avatar src={"../../assets/devitrak_logo.svg"} />}
-                title={<div>{item.deviceType}</div>}
-              />
-            </List.Item>
-          )}
-        />
-        <Grid item xs={12}></Grid>
+        >
+          We are taking you to the current order page for more information.
+          {automaticNavigation()}
+        </Typography>
       </Grid>
-    </>
+      <Grid item xs={12}>
+        <QRCode
+          bgColor="#fff"
+          errorLevel="H"
+          value={
+            qrCodeValue ? qrCodeValue.paymentIntent : "https://devitrak.com"
+          }
+          icon="https://i.ibb.co/kKktFyw/maskable-icon.png"
+        />
+      </Grid>
+      <Grid margin={"1rem auto"} item xs={12}>
+        <Typography
+          textTransform={"none"}
+          color={"var(--gray-900, #101828)"}
+          textAlign={"left"}
+          /* Display xs/Semibold */
+          fontFamily={"Inter"}
+          fontSize={"24px"}
+          fontStyle={"normal"}
+          fontWeight={600}
+          lineHeight={"32px"}
+        >
+          Order completed
+        </Typography>
+      </Grid>
+      <Grid marginX={"auto"} item xs={12}>
+        {" "}
+        <Typography
+          textTransform={"none"}
+          color={"var(--gray-900, #101828)"}
+          textAlign={"center"}
+          /* Display xs/Semibold */
+          fontFamily={"Inter"}
+          fontSize={"14px"}
+          fontStyle={"normal"}
+          fontWeight={400}
+          lineHeight={"20px"}
+          padding={"auto"}
+          style={{
+            textWrap: "balance",
+            width: "80%",
+            margin: "0 auto",
+          }}
+        >
+          Please proceed to go to the pick up location to collect your{" "}
+          {foundTotalDeviceNumber() > 1 ? "devices" : "device"}. Show this qr
+          code to staff to find your order.
+        </Typography>
+      </Grid>
+
+      <Divider />
+      <List
+        style={{
+          width: "90%",
+        }}
+        itemLayout="horizontal"
+        dataSource={currentOrder}
+        renderItem={(item) => (
+          <List.Item
+            actions={[<p key="list-loadmore-more">{item.deviceNeeded}</p>]}
+          >
+            <List.Item.Meta
+              avatar={<Avatar src={"../../assets/devitrak_logo.svg"} />}
+              title={<div><p>{foundTotalDeviceNumber() > 1 ? "Devices" : "Device"} required</p></div>}
+            />
+          </List.Item>
+        )}
+      />
+      <Grid item xs={12}></Grid>
+    </Grid>
   );
 };
 
 export default DisplayQRCode;
+    // const check = await savedStripeTransactions.data.stripeTransactions.filter(
+    //   (transaction) =>
+    //     transaction.paymentIntent === propsToPass.paymentIntentGenerated
+    // );
+    // const checkTransactions = await savedTransactions.data.list.filter(
+    //   (transaction) =>
+    //     transaction.paymentIntent === propsToPass.paymentIntentGenerated
+    // );
+    // if (check.length > 1 && checkTransactions.length > 1) {
+    //   setQrCodeValue(check.at(0));
+    //   devitrackApi.delete(`/stripe/remove-duplicate/${check.at(0).id}`);
+    //   devitrackApi.delete(
+    //     `/transaction/remove-duplicate-transaction/${
+    //       checkTransactions.at(0).id
+    //     }`
+    //   );
+    // }
