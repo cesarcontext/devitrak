dec04319865516939a348beda8504df43a7fc8c9
grodriguezcontextglobal <grodriguez@contextglobal.com>
2023-12-11 23:25:19 -0500

    redirect user to singleSeclection comp all time

--- src/pages/device/DeviceSelection.jsx
+++ src/pages/device/DeviceSelection.jsx
@@ -1,17 +1,13 @@
 import { Suspense } from "react";
-import { useSelector } from "react-redux";
 import LoadingPage from "../loading/LoadingPage";
-import MultipleSelection from "./MultipleSelection";
 import SingleSelection from "./SinglSelection";
 
 const DeviceSelection = () => {
-  const { event } = useSelector((state) => state.event);
   return (
-    <>
-      <Suspense fallback={<LoadingPage />}>
-        {event.deviceSetup?.length > 1 ? <MultipleSelection /> : <SingleSelection />}
-      </Suspense>
-    </>
+    <Suspense fallback={<LoadingPage />}>
+      <SingleSelection />
+    </Suspense>
+
   );
 };
 
