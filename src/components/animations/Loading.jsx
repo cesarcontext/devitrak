12116e344997189ec31e8004afdd947ffca30c13
Gustavo <grodriguez@contextglobal.com>
2024-08-14 16:26:43 -0400

    loading animation component

---
+++ src/components/animations/Loading.jsx
@@ -0,0 +1,46 @@
+import Lottie from "lottie-web";
+import { useEffect, useRef } from "react";
+import animationData from "../../JSON/oWKlmV36kM.json";
+
+const Loading = () => {
+    const container = useRef(null);
+    useEffect(() => {
+        const lottieAnimation = Lottie.loadAnimation({
+            container: container.current,
+            renderer: "svg",
+            loop: true,
+            autoplay: true,
+            animationData: animationData,
+        });
+
+        return () => {
+            lottieAnimation.destroy();
+        };
+    }, []);
+
+    return (
+        <div
+            style={{
+                width: "10vw",
+                height: "10vh",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <div
+                style={{
+                    width: "100%",
+                    height: "100%",
+                    margin: "0 auto",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+                ref={container}
+            ></div>
+        </div>
+    );
+};
+
+export default Loading;
