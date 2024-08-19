6f51cbaabaffd71a2efa8dd8fa3f0182a57fa920
Gustavo <grodriguez@contextglobal.com>
2024-08-19 16:34:09 -0400

    wrapping app with the inactivity log out component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import AuthenticatedRoutes from "./route/AuthenticatedRoutes";
 import "./App.css";
+import InactivityLogout from "./components/utils/InactivityLogout";
 
 function App() {
   return (
-   <AuthenticatedRoutes />
+    <InactivityLogout>
+      <AuthenticatedRoutes />
+    </InactivityLogout>
   );
 }
 
