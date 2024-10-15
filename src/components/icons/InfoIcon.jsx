4f02c680b25e9912bb5a3195f2f1967134fde2a5
Gustavo <grodriguez@contextglobal.com>
2024-10-15 07:42:49 -0400

    new icons

---
+++ src/components/icons/InfoIcon.jsx
@@ -0,0 +1,21 @@
+const InfoIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="29"
+      height="29"
+      viewBox="0 0 29 29"
+      fill="none"
+    >
+      <path
+        d="M14.5 19.1673V14.5007M14.5 9.83398H14.5117M26.1667 14.5007C26.1667 20.944 20.9434 26.1673 14.5 26.1673C8.05672 26.1673 2.83337 20.944 2.83337 14.5007C2.83337 8.05733 8.05672 2.83398 14.5 2.83398C20.9434 2.83398 26.1667 8.05733 26.1667 14.5007Z"
+        stroke="white"
+        strokeWidth="1.66667"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default InfoIcon
---
+++ src/components/icons/ProfileIcon.jsx
@@ -0,0 +1,21 @@
+const ProfileIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="29"
+      height="29"
+      viewBox="0 0 29 29"
+      fill="none"
+    >
+      <path
+        d="M4.3335 23.8333C7.05858 20.943 10.7583 19.1667 14.8335 19.1667C18.9086 19.1667 22.6084 20.943 25.3335 23.8333M20.0835 9.25C20.0835 12.1495 17.733 14.5 14.8335 14.5C11.934 14.5 9.5835 12.1495 9.5835 9.25C9.5835 6.35051 11.934 4 14.8335 4C17.733 4 20.0835 6.35051 20.0835 9.25Z"
+        stroke="white"
+        strokeWidth="1.66667"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default ProfileIcon;
